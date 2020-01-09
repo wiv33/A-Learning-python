bcfb48e03aef2fc4589139d4d1fdf0cf23874386
PSK <wiv821253@gmail.com>
2020-01-09 21:23:03 +0900

    variable

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
 character_name = "John"
 character_age = "31"
 
+isMale = True
 print(character_name)
 print(character_age)
+print(isMale)
 
+print("------------------------------------")
+print    ("이것도 허용해줍니다.")
 character_name = "Mike"
-character_age = "20"
+character_age = 20.52365456345
+isMale = False
 print(character_name)
 print(character_age)
 
