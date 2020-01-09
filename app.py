44f66e8e4de6877f5249f585034c6fe380c5a930
PSK <wiv821253@gmail.com>
2020-01-09 21:33:19 +0900

    variable

--- app.py
+++ app.py
@@ -8,10 +8,33 @@ print(isMale)
 
 print("------------------------------------")
 print    ("이것도 허용해줍니다.")
+
 character_name = "Mike"
 character_age = 20.52365456345
 isMale = False
-print(character_name)
-print(character_age)
+
+# upperCase
+print(character_name.upper())
+
+# Boolean
+print(character_name.upper().isupper())
+
+# Hex
+print(character_age.hex())
+
+# length
+print(len(character_name))
+
+# String start index -> 0
+print(character_name[0])
+
+phrase = "Giraffe Academy"
+# like to indexOf
+print(phrase.index("i"))
+print(phrase.find("A", 0))
+print(phrase.replace("Giraffe", "My Body"))
+
+
+
 
 
