16ba9fddfd648dceaa11567db6d1f97a91abf449
PSK <wiv821253@gmail.com>
2020-01-09 21:45:49 +0900

    variable

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+from math import floor, ceil, sqrt
+
 character_name = "John"
 character_age = "31"
 
@@ -35,6 +37,50 @@ print(phrase.find("A", 0))
 print(phrase.replace("Giraffe", "My Body"))
 
 
+# 연산
+print(3 * 4)
+print(3 + 4.53)
+print(3 * (3 + 2))
+
+print(10 % 3)
+
+myNum = 4
+
+# result 444
+print(str(myNum) * 3)
+
+print(str(myNum) + " my favorite number")
+
+# error
+# unsupported operand type(s) for +: 'int' and 'str'
+# print(myNum + " my favorite number")
 
 
+myNum = -5
+# result : 5
+print(abs(myNum))
+
+# -5 ^ 3
+print(pow(myNum, 3))
+
+# 7
+print(max(4, 7))
+
+print(round(4, 7))
+
+# 3 : 3.0 ~ 3.4
+print(round(3.2))
+
+# 4 : 3.5 ~ 3.9
+print(round(3.5))
+
+# from math import *
+# result : 3 버림
+print(floor(3.7))
+
+# 4 올림
+print(ceil(3.6))
+
+# 6.0
+print(sqrt(36))
 
