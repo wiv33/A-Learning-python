886a6dce87c377e7003c6e72a0ee42ddb6d43e00
PSK <wiv821253@gmail.com>
2020-01-09 21:19:46 +0900

    variable

---
+++ app.py
@@ -0,0 +1,12 @@
+character_name = "John"
+character_age = "31"
+
+print(character_name)
+print(character_age)
+
+character_name = "Mike"
+character_age = "20"
+print(character_name)
+print(character_age)
+
+
