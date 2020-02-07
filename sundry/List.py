bc7bda10142e8c8ea42fd0b815902c17735022ad
PSK <wiv821253@gmail.com>
2020-02-08 07:05:38 +0900

    test

---
+++ sundry/List.py
@@ -0,0 +1,8 @@
+x = [1, 2, 3, 4, 5]
+y = ["hello", "world"]
+z = ["hello", 1, 2, 3]
+
+print(x + y)
+
+
+print(x[0])
---
+++ sundry/main.py
@@ -0,0 +1,9 @@
+print("함수의 시작")
+
+
+def chat(name1, name2):
+    print("%s: hello world" % name1)
+    print("i'm %s" % name2)
+
+
+chat("철수", "영희")
