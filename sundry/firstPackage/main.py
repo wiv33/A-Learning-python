0bd762ebd770938226d2848d24c3e40a6cad33d2
PSK <wiv821253@gmail.com>
2020-02-08 16:20:18 +0900

    inheritance exam

--- sundry/firstPackage/main.py
+++ sundry/firstPackage/main.py
@@ -11,7 +11,20 @@ class Person:
 
 
 
-myNamePrint = Person("print", 20)
+# myNamePrint = Person("print", 20)
 # myNamePrint.say_hello(to_name="브라더")
-myNamePrint.introduce()
+# myNamePrint.introduce()
 
+class Police(Person):
+    def arrest(self, to_arrest):
+        print("넌 체포됐다 %s" % to_arrest)
+
+class Programmer(Person):
+    def program(self, to_program):
+        print("i'm make %s" % to_program)
+
+
+jenny = Police("제임", 17)
+
+jenny.introduce()
+jenny.arrest("모랄")
