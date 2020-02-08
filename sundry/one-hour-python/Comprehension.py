25f1b3debb6b4ff608b1c5c4cd6757914f9f02c9
PSK <wiv821253@gmail.com>
2020-02-08 19:05:17 +0900

    add Comprehension.py

--- sundry/one-hour-python/Comprehension.py
+++ sundry/one-hour-python/Comprehension.py
@@ -1,4 +1,4 @@
-numbers = range(10)
+numbers = range(30)
 
 odd_numbers = []
 
@@ -6,9 +6,16 @@ for num in numbers:
     if num % 2 == 1:
         odd_numbers.append(num)
 
-print(odd_numbers)
+# print(odd_numbers)
 
 ###
 
-odd_numbers = [number for number in numbers if number % 2 == 1]
-print(odd_numbers)
+# odd_numbers = [number for number in numbers if number % 2 == 1]
+# print(odd_numbers)
+
+
+for num in numbers:
+    print(num // 2)
+
+for myString in ['a', 'b', 'c', 'else', 'tdds']:
+    print(myString * 3)
