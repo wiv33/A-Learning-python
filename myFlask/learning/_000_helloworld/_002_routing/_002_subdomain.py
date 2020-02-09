7c6c92c5722866399660ec1f5473b1ba29fbf457
PSK <wiv821253@gmail.com>
2020-02-09 20:57:52 +0900

    redirect 예제 추가, subdomain PathVariable 추가

--- myFlask/learning/_000_helloworld/_002_routing/_002_subdomain.py
+++ myFlask/learning/_000_helloworld/_002_routing/_002_subdomain.py
@@ -13,6 +13,7 @@ app.debug = True
     answer.example.com
     example.com
     example2.com
+    super.example.com
 """
 @app.route("/board", host="example.com")
 @app.route("/board", host="example2.com")
@@ -24,6 +25,22 @@ def board():
 def board_domain_test_and_answer():
     """명확히 확인하고 싶다면 /board 를 /ted, /bod 로 변경해서 확인해보자."""
     return "test, answer 도메인의 board URL을 호출하였습니다."
+
+# http://super.example.com:5000/board
+@app.route("/board", subdomain="<user_domain>")
+def board_sub_domain_path_variable(user_domain):
+    return "{0} 도메인의 /board URL을 호출하였습니다.".format(user_domain)
+
+""" redirect 예제 """
+@app.route("/board_now", redirect_to="/now")
+def board_redirect_to_now():
+    print("body 실행도 되지 않는다.")
+    return "이 구문은 실행되지 않습니다."
+
+@app.route("/now")
+def board_redirect_handle_now():
+    return "redirect 받고 응답합니다."
+
 
 
 if __name__ == '__main__':
