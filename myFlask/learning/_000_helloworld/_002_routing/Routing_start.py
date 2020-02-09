15d4ac6b509efd147d584fd9c8514e69142a747b
PSK <wiv821253@gmail.com>
2020-02-09 19:04:05 +0900

    setting route methods

--- myFlask/learning/_000_helloworld/_002_routing/Routing_start.py
+++ myFlask/learning/_000_helloworld/_002_routing/Routing_start.py
@@ -1,11 +1,23 @@
-from flask import Flask
+from flask import Flask, make_response
 
 app = Flask(__name__)
+
+app.debug = True
+
+myDictionary = {"my_name":"pil_seong", "skill":"hot"}
 
 @app.route("/")
 def hello_world():
     return "Hello World BODY!!"
 
+@app.route("/board", methods=['GET'])
+def board_list():
+    return make_response(('flask framework', 200, {"program": "language python"}))
+
+
+@app.route("/board", methods=['POST'])
+def post_board_list():
+    return make_response("POST BODY")
 
 if __name__ == '__main__':
     app.run()
