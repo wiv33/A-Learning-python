2f4d8545aaf7e596fc17cc059ac7bea8ba9a2cce
PSK <wiv821253@gmail.com>
2020-02-13 23:40:10 +0900

    txt 읽어 실행할 파일 생성

--- sundry/shell-example/RunCommand.py
+++ sundry/shell-example/RunCommand.py
@@ -14,13 +14,10 @@ else:
     target = "ext"
 scriptStr = ""
 for line in open("C:\\dev\\gm_setting\\{0}.txt".format(target), 'r').readlines():
-    scriptStr.join(line)
-    if line is not None:
-        scriptStr.join("&")
+    scriptStr.join(line).join("%")
 
 """ 옵션 중 현재 사용할 옵션
     --noconsole # Dos console off
     -F, --onefile 실행파일 하나로 생성
 """
-os.system("start cmd /c title RUN GM SCRIPT && {}".format(scriptStr))
-
+os.system("start cmd /c title RUN GM SCRIPT && {} exit".format(scriptStr))
