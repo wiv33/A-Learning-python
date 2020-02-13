9714e9a8c5344e40a5d98afcba63d6a59b7a0a09
PSK <wiv821253@gmail.com>
2020-02-14 00:16:06 +0900

    join 수정

--- sundry/shell-example/RunCommand.py
+++ sundry/shell-example/RunCommand.py
@@ -1,4 +1,5 @@
 import os
+
 # 파이썬이 재미있는 이유
 
 # pip install pyinstaller
@@ -6,18 +7,22 @@ import os
 
 # pyinstaller --onefile --icon {iconFile} {pythonFile}
 
-print("중앙일보: a\n매체확장: e\n")
-chk = input()
+chk = input("중앙일보: a\n매체확장: e\n")
+target = ""
 if chk == "a":
     target = "ilbo"
-else:
+elif chk == "e":
     target = "ext"
-scriptStr = ""
-for line in open("C:\\dev\\gm_setting\\{0}.txt".format(target), 'r').readlines():
-    scriptStr.join(line).join("%")
+else:
+    raise "입력 값은 해당사항 없음: [{}]".format(chk)
+
+scriptStr = "echo "
+f = open("C:\\dev\\gm_setting\\{0}.txt".format(target), 'r')
+for line in f.readlines():
+    " ^&^& ".join(line)
 
 """ 옵션 중 현재 사용할 옵션
     --noconsole # Dos console off
     -F, --onefile 실행파일 하나로 생성
 """
-os.system("start cmd /c title RUN GM SCRIPT && {} exit".format(scriptStr))
+os.system("start cmd /c title RUN GM SCRIPT ^&^& {0} pause".format(scriptStr))
