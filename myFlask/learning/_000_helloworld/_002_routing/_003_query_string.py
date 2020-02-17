7e569c4d7353b672c5ec78479beac48252ebe8ca
PSK <wiv821253@gmail.com>
2020-02-17 21:27:06 +0900

    get 함수의 추가 인자 주석

--- myFlask/learning/_000_helloworld/_002_routing/_003_query_string.py
+++ myFlask/learning/_000_helloworld/_002_routing/_003_query_string.py
@@ -5,7 +5,19 @@ app = Flask(__name__)
 # Query를 ?question=answer 로 호출하였다.
 @app.route("/board")
 def board_list():
-    return "쿼리 스트링 question 변수의 값은 {}입니다.".format(request.args.get("question"))
+    return "쿼리 스트링 question 변수의 값은 {}입니다."\
+        .format(request.args.get("question"))
+
+# Get 방식의 Query String
+# /board2?article=10
+@app.route("/board2")
+def board():
+    """
+    request.args.get(:param_key, :default_value, :type)
+    :return:
+    """
+    article_id = request.args.get("article", "2", int)
+    return str(article_id)
 
 
 if __name__ == '__main__':
