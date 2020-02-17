0e8b00c28c32c838726384d2099533f922e11b18
PSK <wiv821253@gmail.com>
2020-02-17 21:22:11 +0900

    queryString 간단예제

--- myFlask/learning/_000_helloworld/_002_routing/_003_query_string.py
+++ myFlask/learning/_000_helloworld/_002_routing/_003_query_string.py
@@ -1,6 +1,11 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
+
+# Query를 ?question=answer 로 호출하였다.
+@app.route("/board")
+def board_list():
+    return "쿼리 스트링 question 변수의 값은 {}입니다.".format(request.args.get("question"))
 
 
 if __name__ == '__main__':
