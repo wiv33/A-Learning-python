dad316a26c495e14332f9ed546b1998327f34005
PSK <wiv821253@gmail.com>
2020-02-17 21:45:01 +0900

    request 객체의 form, values 속성

--- myFlask/learning/_000_helloworld/_002_routing/_003_query_string.py
+++ myFlask/learning/_000_helloworld/_002_routing/_003_query_string.py
@@ -1,14 +1,27 @@
 from flask import Flask, request
+""" request 객체에서 param 꺼내기 예제
+    1. Query String
+      1-1 default 값 정하기
+      1-2 type 정하기
+      
+    2. form submit 시 param 꺼내기
+      2-1 header 설정 주석
+    
+    3. values 속성의 param
+      3-1 CombinedMultiDict :type
+        - MultiDict 데이터 타입들이 합쳐진 형태 
+"""
+
 
 app = Flask(__name__)
 
-# Query를 ?question=answer 로 호출하였다.
+# 1. Query를 ?question=answer 로 호출하였다.
 @app.route("/board")
 def board_list():
     return "쿼리 스트링 question 변수의 값은 {}입니다."\
         .format(request.args.get("question"))
 
-# Get 방식의 Query String
+# 2. Get 방식의 Query String
 # /board2?article=10
 @app.route("/board2")
 def board():
@@ -18,6 +31,23 @@ def board():
     """
     article_id = request.args.get("article", "2", int)
     return str(article_id)
+
+# 3. form submit
+# request.*form*.get 이 되었다.
+# Content-Type : application/x-www-form-urlencoded
+@app.route("/boardForm", methods=["POST"])
+def board_form():
+    article_id = request.form.get("article", "3", int)
+    return str(article_id)
+
+
+# 4. GET과 POST 를 동시에 선언했을 경우
+# GET의 values 값을 가져옴 / GET이 이긴다고 보면 된다.
+@app.route("/boardGetPost", methods=["GET","POST"])
+def board_get_post():
+    return request.values.get("question")
+
+
 
 
 if __name__ == '__main__':
