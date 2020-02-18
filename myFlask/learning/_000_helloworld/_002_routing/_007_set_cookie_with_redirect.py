08f4d1fb0c0f15e1b607f4c909869922b9a32e62
PSK <wiv821253@gmail.com>
2020-02-18 22:26:50 +0900

    add Set Cookie

---
+++ myFlask/learning/_000_helloworld/_002_routing/_007_set_cookie_with_redirect.py
@@ -0,0 +1,22 @@
+﻿from flask import Flask, request, redirect, make_response
+
+app = Flask(__name__)
+
+@app.route("/example/cookie")
+def example_cookie():
+    print(request.cookies)
+    return ""
+
+
+@app.route("/example/cookie_set")
+def example_cookie_set():
+    redirect_to_cookie = redirect("/example/cookie")
+    response = make_response(redirect_to_cookie)
+    response.set_cookie("Cookie Register", value='Example Cookie')
+    return response
+
+
+if __name__ == '__main__':
+    app.run("example.com"
+            ,port=5000,
+            debug=True)
