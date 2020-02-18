6485c37f61447302b20412d374ef1d7df141b3dd
PSK <wiv821253@gmail.com>
2020-02-18 22:00:22 +0900

    add MultiDict

---
+++ myFlask/learning/_000_helloworld/_002_routing/_006_MultiDict.py
@@ -0,0 +1,31 @@
+﻿from werkzeug.datastructures import MultiDict
+
+# Key, String
+post = MultiDict()
+post.add("key", "value")
+
+
+# key, list
+listDict = MultiDict()
+listDict.add("key", ["v1", "v2"])
+
+# default
+defaultDict = MultiDict()
+defaultDict.add("foo", "s")
+defaultDict.setdefault("foo", "my")
+defaultDict.setdefault("lorem", "body")
+
+# result: MultiDict([('foo', 's'), ('lorem', 'body')])
+"""actually
+    if key not in self:
+        self[key] = default
+    else:
+        default = self[key]
+    return default
+"""
+# print(defaultDict)
+
+
+post.clear()
+listDict.clear()
+defaultDict.clear()
