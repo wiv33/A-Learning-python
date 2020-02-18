a5b1f01f1af3b4ce5f7dba736629e7383addc784
PSK <wiv821253@gmail.com>
2020-02-18 23:15:10 +0900

    matplotlib

---
+++ machine-learning/_000_hello_machine/_000_basic/_000_matplotlib/_000_matplotlib.py
@@ -0,0 +1,16 @@
+﻿import numpy as np
+import matplotlib.pyplot as plt
+
+# 학습 데이터 읽기
+train = np.loadtxt('./data/click.csv', delimiter=",", skiprows=1)
+
+## 배열의 슬라이싱 -> [시작:끝]
+## :,1 => 배열 1번 인덱스의 y값을 가져옴.
+train_x = train[:,0]
+train_y = train[:,1]
+
+print(train_y, end=" 끝")
+
+# 점 그래프를 그린다
+plt.plot(train_x, train_y, 'o')
+plt.show()
---
+++ machine-learning/_000_hello_machine/_000_basic/_000_matplotlib/_001_1차함수_구현.py
@@ -0,0 +1,37 @@
+﻿import numpy as np
+import matplotlib.pyplot as plt
+
+"""이번 코드로 가로축 수치가 작아진다.
+"""
+
+
+# 학습 데이터 읽기
+train = np.loadtxt('./data/click.csv', delimiter=",", skiprows=1)
+
+train_x = train[:,0]
+train_y = train[:,1]
+
+theta0 = np.random.rand()
+theta1 = np.random.rand()
+
+# 예측함수
+def f(x):
+    return theta0 + theta1 * x
+
+# 목적함수
+def E(x, y):
+    return 0.5 * np.sum((y - f(x))**2)
+
+# 표준화
+mu = train_x.mean() # 평균
+sigma = train_x.std()
+
+
+def standardize(x):
+    return (x - mu) / sigma
+
+train_z = standardize(train_x)
+
+# 점 그래프를 그린다
+plt.plot(train_z, train_y, 'o')
+plt.show()
