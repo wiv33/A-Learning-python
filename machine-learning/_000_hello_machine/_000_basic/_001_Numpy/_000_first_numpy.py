81dbc0d0cbfbdff894a70ed95414448c84929f3b
PSK <wiv821253@gmail.com>
2020-02-20 21:14:23 +0900

    numpy 학습

---
+++ machine-learning/_000_hello_machine/_000_basic/_001_Numpy/_000_first_numpy.py
@@ -0,0 +1,17 @@
+﻿import numpy as np
+
+# Array
+mathScoreNDarray = np.array([[11, 12, 13], [21,22,23], [31,32,33]])
+# print(mathScoreNDarray)
+
+print(mathScoreNDarray + 1)
+
+sum = np.sum(mathScoreNDarray)
+print(sum)
+
+average = np.average(mathScoreNDarray)
+print(average)
+
+mean = np.mean(mathScoreNDarray, axis=1)
+print(mean)
+
