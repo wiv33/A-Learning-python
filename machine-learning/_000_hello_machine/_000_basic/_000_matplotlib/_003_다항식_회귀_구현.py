a3b6bd1904d71087f9bc0f2aa00815b37a602971
PSK <wiv821253@gmail.com>
2020-02-20 21:41:34 +0900

    그래프의 변형 중 오류성 데이터 확인 필요

---
+++ machine-learning/_000_hello_machine/_000_basic/_000_matplotlib/_003_다항식_회귀_구현.py
@@ -0,0 +1,56 @@
+﻿import numpy as np
+import matplotlib.pyplot as plt
+
+train = np.loadtxt('./data/click.csv', delimiter=",", skiprows=1)
+
+train_x = train[:,0]
+train_y = train[:,1]
+
+# 목적함수
+def E(x, y):
+    return 0.5 * np.sum((y - f(x))**2)
+
+# 표준화
+mu = train_x.mean() # 평균
+sigma = train_x.std()
+
+def standardize(x):
+    return (x - mu) / sigma
+
+train_z = standardize(train_x)
+
+# 매개변수 초기화
+theta = np.random.rand(3)
+
+# 학습 데이터를 행렬의 형태로 만든다
+def to_matrix(x):
+    return np.vstack([np.ones(x.shape[0]), x, x**2]).T
+
+X = to_matrix(train_z)
+
+# 예측함수
+def f(x):
+    return np.dot(x, theta)
+
+# 학습률
+ETA = 1e-3 # 10^-3
+
+#오차의 차분
+diff = 1
+
+# 학습을 반복한다.
+error = E(X, train_x)
+
+while diff > 1e-2:
+    # 매개변수를 갱신한다.
+    theta = theta - ETA * np.dot(f(X) - train_y, X)
+    # 이전의 오차와의 차분을 계산
+    current_error = E(X, train_y)
+    diff = error - current_error
+    error = current_error
+
+x = np.linspace(-3, 3, 100)
+
+plt.plot(train_z, train_y, 'o')
+plt.plot(x, f(to_matrix(x)))
+plt.show()
