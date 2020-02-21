16d5295d997ca4d030ea535390a5959cb1aa6dfa
PSK <wiv821253@gmail.com>
2020-02-21 20:58:11 +0900

    numpy 통계 - ptp, median

---
+++ machine-learning/_000_hello_machine/_000_basic/_001_Numpy/_006_통계_ptp_median.py
@@ -0,0 +1,18 @@
+﻿import numpy as np
+
+A = np.array([[5, 2, 6], [1, 4, 9], [3, 7, 8]])
+
+# ptp 배열의 최대와 최소 값의 차분
+# print(np.ptp(A))  #  위 배열의 최대와 최소의 차이는 7
+
+# print(np.median(A))  # A의 중간 값
+# print(np.median(A, axis=0))  # axis=0 중간 값 [3. 4. 8.]
+
+print(np.median(A, axis=1))  # axis=1 중간 값 [5. 4. 7.]
+
+# arithmetic mean
+print(np.mean(A, 0))
+print(np.mean(A, 1))
+print(np.mean(A))
+
+# 가중 평균을 사용하려면 average를 사용해야 한다.
