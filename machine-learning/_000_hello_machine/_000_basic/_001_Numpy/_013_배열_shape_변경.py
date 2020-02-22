c3f1dbd3f06361fee9800a61497bdeee695959a3
PSK <wiv821253@gmail.com>
2020-02-22 13:28:01 +0900

    array shape, concatenate

---
+++ machine-learning/_000_hello_machine/_000_basic/_001_Numpy/_013_배열_shape_변경.py
@@ -0,0 +1,60 @@
+﻿import numpy as np
+
+"""차원의 변경
+np.arange(6)
+# result
+[0,1,2,3,4,5]
+
+.reshape((3,2))
+# result
+array([[0, 1],
+       [2, 3],
+       [4, 5]])
+"""
+a = np.arange(6).reshape((3, 2))
+
+np.reshape(a, (2, 3))
+# result
+# array([[0, 1, 2],
+#        [3, 4, 5]])
+
+np.reshape(a, (6, 1))
+# result
+# array([[0],
+#        [1],
+#        [2],
+#        [3],
+#        [4],
+#        [5]])
+
+np.reshape(a, (1, 6))
+# result
+# array([[0, 1, 2, 3, 4, 5]])
+
+np.reshape(a, 6)
+# result : 차원이 낮아진다.
+# array([0, 1, 2, 3, 4, 5])
+
+
+# ============================================ ravel ==========================================
+
+np.ravel(a)
+# result
+# array([0, 1, 2, 3, 4, 5])
+
+a.ravel()
+# result
+# array([0, 1, 2, 3, 4, 5])
+
+
+# ===== flatten
+# deep copy
+np.ndarray.flatten(a)
+# result
+# array([0, 1, 2, 3, 4, 5])
+
+# 위와 같음
+a.flatten()
+# a.flatten()
+
+
---
+++ machine-learning/_000_hello_machine/_000_basic/_001_Numpy/_014_배열_concatenate.py
@@ -0,0 +1,28 @@
+﻿import numpy as np
+
+a = np.array([[1, 2], [3, 4], [5, 6], [7, 8]])
+b = np.array([[9, 10, 11, 12]])
+
+""" 자주하는 실수 **
+기존 배열의 인덱싱 후 
+transpose 하려면 1차원이라 되지 않는다.
+"""
+
+# 배열의 형태가 같아야 한다.
+# transpose 후 연결할 수 있다.
+np.concatenate((a,b), axis=0)
+# result :
+# all the input array dimensions for the concatenation axis must match exactly,
+# but along dimension 1, the array at index 0 has size 2 and the array at index 1 has size 4
+
+np.concatenate((a.T, b), axis=0)
+# array([[ 1,  3,  5,  7],
+#        [ 2,  4,  6,  8],
+#        [ 9, 10, 11, 12]])
+
+np.concatenate((a, b.T), axis=1)
+# result
+# array([[ 1,  2,  9],
+#        [ 3,  4, 10],
+#        [ 5,  6, 11],
+#        [ 7,  8, 12]])
