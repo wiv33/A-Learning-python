af5b2aad700bb50c452ee7c208567c5be75439ec
PSK <wiv821253@gmail.com>
2020-02-22 13:01:40 +0900

    array

---
+++ machine-learning/_000_hello_machine/_000_basic/_001_Numpy/_010_배열_bool_indexed.py
@@ -0,0 +1,23 @@
+﻿import numpy as np
+
+a = np.array([[1, 2], [3, 4], [5, 6], [7, 8]])
+
+bool_a = a > 2
+# result
+# >>> bool_a
+# array([[False, False],
+#        [ True,  True],
+#        [ True,  True],
+#        [ True,  True]])
+
+a[bool_a]
+# result
+# >>> a[bool_a]
+# array([3, 4, 5, 6, 7, 8])
+
+# ===================================================
+
+print(a[a>3], a[a>3].shape, a[a>3].ndim)
+# result
+# [4 5 6 7 8] (5,) 1
+
