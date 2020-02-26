dea6bd8302bf2aad98405c0a4ed39e54a30c23d4
PSK <wiv821253@gmail.com>
2020-02-26 22:52:30 +0900

    LinearRegression

---
+++ machine-learning/_000_hello_machine/_000_basic/_002_Simple_linear_Regression/_000_/_000_SimpleLinearRegression.py
@@ -0,0 +1,12 @@
+﻿import numpy as np
+import tensorflow as tf
+
+x_data = range(1, 6)
+y_data = range(1, 6)
+
+W = tf.Variable(2.9)
+b = tf.Variable(0.5)
+
+# hypothesis = W * x + b
+hypothesis = W * x_data + b
+
