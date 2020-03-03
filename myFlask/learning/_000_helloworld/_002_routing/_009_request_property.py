fd53d6de5e91dc29c7669f6d112a3008bdfea8dd
PSK <wiv821253@gmail.com>
2020-03-03 23:59:22 +0900

    flask request property

---
+++ myFlask/learning/_000_helloworld/_002_routing/_009_request_property.py
@@ -0,0 +1,20 @@
+﻿from flask import Flask, request
+
+app = Flask(__name__)
+
+
+@app.route("/example/environ", methods=["GET", "POST"])
+def example_environ():
+    return ("path: %s<br/>"
+            "script_root: %s<br/>"
+            "url: %s<br/>"
+            "base_url: %s<br/>"
+            "url_root: %s<br/>") % (request.path, request.script_root,
+                                    request.url, request.base_url,
+                                    request.url_root)
+
+
+if __name__ == '__main__':
+    app.run("example.com",
+            5000,
+            True)
