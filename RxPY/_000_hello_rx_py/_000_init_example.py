e5ce8afc2e5a44f2eb30dc3e187e67c6b34a5b6c
PSK <wiv821253@gmail.com>
2020-03-04 00:29:46 +0900

    add rxPy

---
+++ RxPY/_000_hello_rx_py/_000_init_example.py
@@ -0,0 +1,11 @@
+﻿import rx
+from rx import operators as ops
+
+source = rx.of("Alpha", "Beta", "Gamma", "Delta", "Epsilon")
+
+composed = source.pipe(
+    ops.map(lambda s: len(s)),
+    ops.filter(lambda i: i >= 5)
+)
+
+composed.subscribe(lambda value: print("Received {0}".format(value)))
