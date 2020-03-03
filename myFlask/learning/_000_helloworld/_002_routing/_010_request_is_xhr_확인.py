b2bc698403d29edf8d816c687db31e4a363b1023
PSK <wiv821253@gmail.com>
2020-03-04 00:20:13 +0900

    flask request property

---
+++ myFlask/learning/_000_helloworld/_002_routing/_010_request_is_xhr_확인.py
@@ -0,0 +1,13 @@
+﻿from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route("/example/environ", methods=['GET', 'POST'])
+def example_environ_is_xhr():
+    print(request.is_xhr)
+    return ""
+
+if __name__ == '__main__':
+    app.run("example.com",
+            5000,
+            True)
