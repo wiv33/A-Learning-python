fb5de8a5569f94dd52adee65fd5ced5e43ecb3d7
PSK <wiv821253@gmail.com>
2020-03-05 21:26:02 +0900

    used create method

---
+++ RxPY/_000_hello_rx_py/_002_creat_observables.py
@@ -0,0 +1,15 @@
+﻿from rx import create
+
+
+def test_observable(observer, scheduler):
+    observer.on_next("Hello")
+    # observer.on_error("Error")
+    observer.on_completed()
+
+
+source = create(test_observable)
+source.subscribe(
+    on_next=lambda i: print("Got - {0}".format(i)),
+    on_error=lambda e: print("Error : {0}".format(e)),
+    on_completed=lambda: print("Job Done!"),
+)
