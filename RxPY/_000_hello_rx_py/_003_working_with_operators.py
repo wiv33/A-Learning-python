ea0bd51f1a9494a33293a270d371c8d6d67f5857
PSK <wiv821253@gmail.com>
2020-03-05 21:45:25 +0900

    reduce 풀어쓰기

---
+++ RxPY/_000_hello_rx_py/_003_working_with_operators.py
@@ -0,0 +1,15 @@
+﻿from rx import range, operators as op
+
+test = range(1, 11)
+
+sub1 = test.pipe(
+    op.filter(lambda s: s % 2 == 0), # next x -> 2, 4, 6, 8, 10
+    op.reduce(lambda acc, x: acc + x, 3)
+    # acc: 3, x: 2 -> 5
+    # acc: 5, x: 4 -> 9
+    # acc: 9, x: 6 -> 15
+    # acc: 15, x: 8 -> 23
+    # acc: 23, x: 10 -> 33
+)
+
+sub1.subscribe(lambda x: print("This value {}".format(x)))
