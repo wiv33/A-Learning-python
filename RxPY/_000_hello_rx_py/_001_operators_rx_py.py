06cf9cf9de479c3aaacb123e9053a172bf121711
PSK <wiv821253@gmail.com>
2020-03-05 21:13:23 +0900

    add Operators pip chaining

--- RxPY/_000_hello_rx_py/_001_operators_rx_py.py
+++ RxPY/_000_hello_rx_py/_001_operators_rx_py.py
@@ -1,6 +1,12 @@
 ﻿import rx
 from rx import operators as ops
 
-pipe = rx.range(1, 11).pipe(ops.filter(lambda i: i % 2 == 0), ops.sum())
+pipe = rx.range(1, 11) \
+    .pipe(ops.filter(lambda i: i % 2 == 0),
+          ops.filter(lambda i: i < 3),
+          ops.map(lambda i: i + i),
+          ops.map(lambda i: i ** i),
+          ops.average(lambda x: x + x),
+          ops.sum())
 
 pipe.subscribe(lambda x: print("value is {0}".format(x)))
