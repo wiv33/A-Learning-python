fc2fc9bb619d9be2bfc185b7ac477b062747feae
PSK <wiv821253@gmail.com>
2020-03-07 19:37:58 +0900

    repeat_value

---
+++ RxPY/_000_hello_rx_py/_005_rx_creating_operators/_008_repeat_value.py
@@ -0,0 +1,33 @@
+﻿from rx import repeat_value
+
+"""
+this method will create an observable that will repeat the given value as per the count is given
+
+:parameter
+    value: optional. the value to be repeated
+    repeat_count: optional. the number of times the given value to be repeated.
+
+:return
+    it will return an observable the will repeat the given value as per the count is given.
+"""
+
+my_repeat = repeat_value(77, 10)
+
+my_repeat.subscribe(
+    lambda x: print("value is {}".format(x)),
+    lambda e: print("error : {}".format(e)),
+    lambda : print("Complete")
+)
+
+# result
+# value is 77
+# value is 77
+# value is 77
+# value is 77
+# value is 77
+# value is 77
+# value is 77
+# value is 77
+# value is 77
+# value is 77
+# Complete
