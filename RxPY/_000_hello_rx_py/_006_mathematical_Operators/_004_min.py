2415b52cca740ea7e288053c93f04e8118ed0e84
PSK <wiv821253@gmail.com>
2020-03-07 21:45:49 +0900

    min

---
+++ RxPY/_000_hello_rx_py/_006_mathematical_Operators/_004_min.py
@@ -0,0 +1,21 @@
+﻿"""this operator will give an observable with min value from the source observable
+
+:parameter
+    comparer_function: optional param.
+    this function is used on source observables to compare values.
+
+:return
+    it returns an observable with min value from the source observable.
+
+"""
+
+from rx import of, operators as op
+
+o = of(12, 32, 41, 52, 62, 502, 555)
+
+o.pipe(
+    # op.min(lambda t1, t2: t1 - t2), # Min value is 12
+    # op.min(lambda t1, t2: t2 - t1) # Min value is 555
+    op.min() # Min value is 12
+).subscribe(lambda x: print("Min value is {}".format(x)))
+
