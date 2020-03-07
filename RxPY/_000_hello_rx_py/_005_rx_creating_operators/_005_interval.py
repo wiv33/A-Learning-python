b41ace6a5ade47622e073b85e8140ae4ca2f0307
PSK <wiv821253@gmail.com>
2020-03-07 18:57:14 +0900

    interval, just

---
+++ RxPY/_000_hello_rx_py/_005_rx_creating_operators/_005_interval.py
@@ -0,0 +1,18 @@
+﻿from rx import interval, operators as ops
+
+"""
+this method will give a series of values produced after a timeout.
+
+:parameter
+    period: to start the Number sequence
+    
+:return value
+    it returns an observable with all the values in sequential order
+"""
+
+my_interval = interval(1.2).pipe(
+    ops.map(lambda i: i * i),
+    # ops.map(lambda x: x + " x") ## TypeError: unsupported operand type(s) for +: 'int' and 'str'
+).subscribe(lambda x: print("value is {}".format(x)))
+
+input("press any key to exit\n")
---
+++ RxPY/_000_hello_rx_py/_005_rx_creating_operators/_006_just.py
@@ -0,0 +1,31 @@
+﻿from rx import just
+
+"""
+:parameter
+    value: to be converted to an observable
+    
+:return
+    it will return an observable with the given values
+"""
+
+my_just = just((10, 52, 34, 55, 81))
+
+my_just.subscribe(
+    lambda x: print("value is {}".format(x)),
+    lambda e: print("error : {}".format(e)),
+    lambda : print("complete")
+)
+# result
+# value is (10, 52, 34, 55, 81)
+# complete
+
+my_just_dict = just(dict({"Key": "my Body", "value": "is"}))
+
+my_just_dict.subscribe(
+    lambda x: print("value is {}".format(x)),
+    lambda e: print("error : {}".format(e)),
+    lambda : print("complete")
+)
+# result
+# value is {'Key': 'my Body', 'value': 'is'}
+# complete
