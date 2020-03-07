70ed2dfd278a5a30adef44cbdf034fbc7f78e6a8
PSK <wiv821253@gmail.com>
2020-03-07 19:46:00 +0900

    start

---
+++ RxPY/_000_hello_rx_py/_005_rx_creating_operators/_009_start.py
@@ -0,0 +1,28 @@
+﻿from rx import start
+
+"""
+this method takes in a function as an input, and returns an observable 
+that will return value from the input function
+
+:parameter
+    func: a function that will be called.
+    
+:return
+    it returns an observable that will have a return value from the input function.
+"""
+lambdas = [lambda: "Hello RxPy", lambda: 5 * 7]
+
+for item in lambdas:
+    my_start = start(item)
+
+    my_start.subscribe(
+        lambda x: print("value is {}".format(x)),
+        lambda e: print("error {}".format(e)),
+        lambda: print("Job Done!")
+    )
+
+# result
+# value is Hello RxPy
+# Job Done!
+# value is 35
+# Job Done!
