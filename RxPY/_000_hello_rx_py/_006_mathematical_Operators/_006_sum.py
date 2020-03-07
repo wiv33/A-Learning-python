fb0f048a932f914e24b3708a472ff0c68b9db6dd
PSK <wiv821253@gmail.com>
2020-03-07 22:30:39 +0900

    sum

---
+++ RxPY/_000_hello_rx_py/_006_mathematical_Operators/_006_sum.py
@@ -0,0 +1,18 @@
+﻿"""this operator will return an observable with the sum of all the values from source observables.
+
+:parameter
+    key_mapper: optional. this is the function,
+    that is applied to the values coming from the source observable.
+
+:return
+    it returns an observable with the sum of all the values from the source observable.
+
+"""
+
+from rx import of, operators as op
+
+of(1,2,3,4,5,6,7,8,9,10)\
+    .pipe(
+    # op.sum() # the sum is 55
+    op.sum(lambda n: n + 1) # the sum is 65
+).subscribe(lambda x: print("the sum is {}".format(x)))
