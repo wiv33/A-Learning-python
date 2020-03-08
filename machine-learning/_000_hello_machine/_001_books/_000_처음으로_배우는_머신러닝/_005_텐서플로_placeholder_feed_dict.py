cb66cbda796cf49c81c46b50856ada67ada684c9
PSK <wiv821253@gmail.com>
2020-03-08 20:36:29 +0900

    books - placeholder_feed_dict

---
+++ machine-learning/_000_hello_machine/_001_books/_000_처음으로_배우는_머신러닝/_005_텐서플로_placeholder_feed_dict.py
@@ -0,0 +1,20 @@
+﻿import tensorflow as tf
+"""
+placeholder, feed_dict
+
+텐서플로 연산 그래프에 입력 데이터를 넣기 위해 사용된다.
+
+placeholder:
+    변수 -> 구체적인 형태나 값이 정해지지 않은 임시 변수
+feed_dict:
+    플레이스홀더(placeholder)와 실제값을 연결하는 역할을 한다.
+    placeholder 안에 값을 채워 넣는다.
+
+"""
+with tf.Session() as session:
+    input1 = tf.placeholder(tf.float32, (3,2)) # :type, :shape
+    input2 = tf.placeholder(tf.float32)
+
+    output = tf.multiply(input1, input2) # 두 입력을 곱셈한다.
+
+    print(session.run([output], feed_dict={input1:[[1.,2.],[4.,5.],[6.,7.]], input2:[3., 2]}))
