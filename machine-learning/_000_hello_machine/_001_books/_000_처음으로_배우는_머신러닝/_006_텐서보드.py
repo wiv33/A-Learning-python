6b3ba9b8e490a27ba2d81371b8de9c87c3445012
PSK <wiv821253@gmail.com>
2020-03-08 23:52:59 +0900

    books - tensorflow boston

---
+++ machine-learning/_000_hello_machine/_001_books/_000_처음으로_배우는_머신러닝/_006_텐서보드.py
@@ -0,0 +1,27 @@
+﻿"""
+텐서보드:
+    연산 그래프와 파라미터를 시각화하여 학습 과정을 모니터링하거나
+    디버깅하는 데 도움을 준다.
+
+흐름
+    1. 그래프 생성 -> FileWriter(생성한 그래프)
+    2. tf.scalar_summary 클래스에 추적하고 싶은 값(ex.각 학습 시의 손실)을 인자로 갖는
+    기록함수를 생성하고, 세선이 시작된 후 SummaryWriter 함수에 그 함수를 인자로 넣어 기록하는 방법
+
+결과 출력
+     텐서보드를 호출
+     terminal
+        tensorboard --logidr=./logs/fit
+"""
+
+import tensorflow as tf
+
+session = tf.InteractiveSession()
+a = tf.constant(2, name='a')
+b = tf.constant(3, name='b')
+x = tf.add(a, b)
+writer = tf.summary.FileWriter('./logs/fit', session.graph)
+session.run(x)
+writer.close()
+session.close()
+
---
+++ machine-learning/_000_hello_machine/_001_books/_000_처음으로_배우는_머신러닝/_007_텐서_선형_회귀_Boston.py
@@ -0,0 +1,36 @@
+﻿"""
+ref: _000_Boston_dataset
+
+학습과정
+    1. 데이터 입력을 위한 플레이스홀더 정의
+    2. 학습에 사용할 파라미터를 변수형으로 정의
+    3. 모델, 손실함수, 최적화함수 정의
+    4. 세션 시작
+    5. 변수 초기화 선언
+    6. 데이터 피딩
+    7. 최적화함수 실행
+
+    *시각화
+"""
+import tensorflow as tf
+import numpy as np
+from matplotlib import pyplot as plt  # 학습 결과 플롯용 패키지
+from sklearn import datasets
+
+boston = datasets.load_boston()
+boston_slice = [x[5] for x in boston.data]
+
+# 텐서플로에서 사용할 크기로 조정
+# reshape 함수로 데이터를 열이 1인 배열형으로 변환
+data_x = np.array(boston_slice).reshape(-1, 1)  # == np.array(boston_slice).reshape(506, 1)
+data_y = boston.target.reshape(-1, 1)
+# data_x.shape == data_y.shape: True
+
+""" 
+선형 회귀
+    y = wx + b
+    입력 데이터(x) * 기울기(w) + 편향(b, 절편이라고도 한다) = 타깃 데이터(y)가 되는 
+    기울기와 편향을 찾는 것이 목표 
+    
+학습 전에는 기울기와 편향을 알 수 없기 때문에 0으로 설정한 후 최적화를 통해 학습한다.
+"""
