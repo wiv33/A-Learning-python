0da253e018ab52be34f99190b04f736fa2e066f1
PSK <wiv821253@gmail.com>
2020-03-10 00:38:15 +0900

    books - init code

---
+++ machine-learning/_000_hello_machine/_001_books/_001_처음으로_배우는_군집화_기법/_001_데이터_기초_통계량_계산__시각화.py
@@ -0,0 +1,12 @@
+﻿import time
+from scipy import stats
+from openpyxl import load_workbook
+
+
+user_product_dict = {}
+product_user_dict = {}
+
+wb = load_workbook("./_000_Online_Retail.xlsx", data_only=True)
+ws = wb['Online Retail']
+
+
