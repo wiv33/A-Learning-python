e33fb752557a1481c6d32f03ef9b880fe53332e2
PSK <wiv821253@gmail.com>
2020-03-11 13:59:24 +0900

    result

--- machine-learning/_000_hello_machine/_001_books/_001_처음으로_배우는_군집화_기법/_001_데이터_기초_통계량_계산__시각화.py
+++ machine-learning/_000_hello_machine/_001_books/_001_처음으로_배우는_군집화_기법/_001_데이터_기초_통계량_계산__시각화.py
@@ -1,7 +1,9 @@
 ﻿import time
-from scipy import stats
+
+import pandas as pd
+import xlrd
 from openpyxl import load_workbook
-import pandas as pd, xlrd
+from scipy import stats
 
 # 데이터 구조 정의
 # 사용자 ID를 키로 갖고 상품 코드의 셋을 값으로 갖는 딕셔너리와
@@ -16,20 +18,25 @@ product_id_name_dict = {}
 # wb = load_workbook("./_000_Online_Retail.xlsx", data_only=True)
 # ws = wb['Online Retail']
 excel = pd.read_excel("./_000_Online_Retail.xlsx", header=1)
-for i in excel.iterrows():
-    print(i)
-    user_code = excel[i]
-    product_id = stockCode
-    product_name = Description
+for i, v in excel.iterrows():
+    user_code = v.values[6]
+    product_id = v.values[1]
+    product_name = v.values[2]
+    country = v.values[7]
 
     # 유저 코드가 없거나 영국이 아닐 경우
-    if len(user_code) <= 0 or country != 'United Kingdom':
+    if (str(user_code) is "nan" or user_code <= 0) \
+            or country != 'United Kingdom':
+        print(user_code)
+        print(country)
         continue
 
+    invoiceDate = v.values[4]
     # 연도 에러 처리
     try:
-        invoice_year = time.strptime(invoiceDate, '%m/%d/%y %H:%M').tm_year
+        invoice_year = invoiceDate.year
     except ValueError:
+        print("Occurred Error: {}".format(invoiceDate))
         continue
 
     if invoice_year != 2011:
@@ -52,3 +59,10 @@ print("# of products: ", len(product_user_dict))
 
 # 각 사용자가 구매한 상품 가짓수로 기초 통계량을 출력.
 print(stats.describe(product_per_user_li))
+
+# result
+# # of users:  121846
+# # of products:  3989
+# DescribeResult(
+# nobs=121846, minmax=(1, 1603), mean=2.815767444150813, variance=296.7709659615601,
+# skewness=25.03342231615607, kurtosis=1310.234261310257)
