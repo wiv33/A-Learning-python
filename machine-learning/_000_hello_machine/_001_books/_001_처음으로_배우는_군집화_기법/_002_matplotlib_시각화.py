0681df83f874505fe5956398266545acd85f2ddc
PSK <wiv821253@gmail.com>
2020-03-12 21:41:54 +0900

    book - add

--- machine-learning/_000_hello_machine/_001_books/_001_처음으로_배우는_군집화_기법/_002_matplotlib_시각화.py
+++ machine-learning/_000_hello_machine/_001_books/_001_처음으로_배우는_군집화_기법/_002_matplotlib_시각화.py
@@ -1,8 +1,8 @@
 ﻿import pandas as pd
-from openpyxl import load_workbook
+import matplotlib.pyplot as plt
+from collections import Counter
 from scipy import stats
 
-# 데이터 구조 정의
 # 사용자 ID를 키로 갖고 상품 코드의 셋을 값으로 갖는 딕셔너리와
 # 상품 코드를 키로 갖고 사용자 ID의 셋을 값으로 갖는 딕셔너리
 user_product_dict = {}
@@ -12,8 +12,6 @@ product_user_dict = {}
 # 군집화의 내용을 확인하는 단계에서 상품명을 사용한다.
 product_id_name_dict = {}
 
-# wb = load_workbook("./_000_Online_Retail.xlsx", data_only=True)
-# ws = wb['Online Retail']
 excel = pd.read_excel("./_000_Online_Retail.xlsx", header=1)
 items = excel.dropna() # row에 NaN column 이 한개 이상 포함되어 있을 경우 제외시킨다.
 for i, v in items.iterrows():
@@ -48,20 +46,14 @@ for i, v in items.iterrows():
 
 # 각 사용자가 구매한 상품 가짓수로 리스트 생성
 product_per_user_li = [len(x) for x in user_product_dict.values()]
-
-print("# of users: ", len(user_product_dict))
-print("# of products: ", len(product_user_dict))
-
 # 각 사용자가 구매한 상품 가짓수로 기초 통계량을 출력.
-print("============ 1 ==============")
-print(user_product_dict)
-print("============ 2 ==============")
-print(product_user_dict)
-print("============ 3 ==============")
 print(stats.describe(product_per_user_li))
 
-# result
-# # of users:  3835
-# # of products:  3588
-# DescribeResult(nobs=3835, minmax=(1, 1603), mean=58.69074315514993, variance=6207.1088214350575,
-# skewness=5.833816723404396, kurtosis=72.60364097540136)
+plot_data_all = Counter(product_per_user_li)
+plot_data_x = list(plot_data_all.keys())
+plot_data_y = list(plot_data_all.values())
+plt.xlabel('고유 상품 가짓수')
+plt.ylabel('사용자 수')
+plt.scatter(plot_data_x, plot_data_y, marker="+")
+
+plt.show()
--- machine-learning/_000_hello_machine/_001_books/_001_처음으로_배우는_군집화_기법/_003_예외_패턴_사용자_제거.py
+++ machine-learning/_000_hello_machine/_001_books/_001_처음으로_배우는_군집화_기법/_003_예외_패턴_사용자_제거.py
@@ -1,8 +1,8 @@
 ﻿import pandas as pd
-from openpyxl import load_workbook
+import matplotlib.pyplot as plt
+from collections import Counter
 from scipy import stats
 
-# 데이터 구조 정의
 # 사용자 ID를 키로 갖고 상품 코드의 셋을 값으로 갖는 딕셔너리와
 # 상품 코드를 키로 갖고 사용자 ID의 셋을 값으로 갖는 딕셔너리
 user_product_dict = {}
@@ -12,8 +12,6 @@ product_user_dict = {}
 # 군집화의 내용을 확인하는 단계에서 상품명을 사용한다.
 product_id_name_dict = {}
 
-# wb = load_workbook("./_000_Online_Retail.xlsx", data_only=True)
-# ws = wb['Online Retail']
 excel = pd.read_excel("./_000_Online_Retail.xlsx", header=1)
 items = excel.dropna() # row에 NaN column 이 한개 이상 포함되어 있을 경우 제외시킨다.
 for i, v in items.iterrows():
@@ -49,19 +47,29 @@ for i, v in items.iterrows():
 # 각 사용자가 구매한 상품 가짓수로 리스트 생성
 product_per_user_li = [len(x) for x in user_product_dict.values()]
 
-print("# of users: ", len(user_product_dict))
-print("# of products: ", len(product_user_dict))
+# 상품 가짓수가 1인 사용자 id
+min_product_user_li = [k for k, v in user_product_dict.items() if len(v) == 1]
 
-# 각 사용자가 구매한 상품 가짓수로 기초 통계량을 출력.
-print("============ 1 ==============")
-print(user_product_dict)
-print("============ 2 ==============")
-print(product_user_dict)
-print("============ 3 ==============")
-print(stats.describe(product_per_user_li))
+# 구매한 상품의 가짓수가 600개 이상인 사용자 id
+max_product_user_li = [k for k, v in user_product_dict.items() if len(v) >= 600]
 
-# result
-# # of users:  3835
-# # of products:  3588
-# DescribeResult(nobs=3835, minmax=(1, 1603), mean=58.69074315514993, variance=6207.1088214350575,
-# skewness=5.833816723404396, kurtosis=72.60364097540136)
+print("# of users purchased one product:%d" % (len(min_product_user_li)))
+print("# of users purchased more than 600 product:%d" % (len(max_product_user_li)))
+
+
+# 찾아낸 사용자를 군집화에 사용할 user_product_dict 에서 제회
+user_product_dict = {k:v for k, v in user_product_dict.items() if 1 < len(v) <= 600}
+
+print("# of left user:%d" % len(user_product_dict))
+
+# 남아있는 사용자가 구매한 상품에서도 0에서 시작하는 고유 ID를 부여한다.
+# 데이터셋에서 지외된 사용자가 구매한 상품은 군집화에서 사용하지 않기 때문에 이러한 처리를 한다.
+id_product_dict = {}
+for product_set_li in user_product_dict.values():
+    for x in product_set_li:
+        if x in id_product_dict:
+            product_id = id_product_dict[x]
+        else:
+            id_product_dict.setdefault(x, len(id_product_dict))
+
+print("# of left items:%d" % len(id_product_dict))
