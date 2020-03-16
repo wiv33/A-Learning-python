93f7d947eae66f95d7c32cd3c8be718268415e05
PSK <wiv821253@gmail.com>
2020-03-16 23:39:24 +0900

    rxpy group_by, from_

---
+++ RxPY/_000_hello_rx_py/_007_transformation_Operators/_001_group_by.py
@@ -0,0 +1,24 @@
+﻿"""the operator will group values coming from the source observable based
+on the key_mapper function given
+
+:parameter
+    key_mapper: this function will take care of
+
+:return
+    it returns an observable with values grouped based on the key_mapper function.
+
+"""
+
+from rx import from_, operators as op
+from_(['A', 'B', 'C', 'D', 'E', 'F'])\
+    .pipe(
+    op.group_by(lambda v: v[0])
+).subscribe(lambda x: print("the element is {}".format(x)))
+
+# result
+# the element is <rx.core.observable.groupedobservable.GroupedObservable object at 0x0000022B34BECF08>
+# the element is <rx.core.observable.groupedobservable.GroupedObservable object at 0x0000022B34BECFC8>
+# the element is <rx.core.observable.groupedobservable.GroupedObservable object at 0x0000022B34BECFC8>
+# the element is <rx.core.observable.groupedobservable.GroupedObservable object at 0x0000022B34BF1888>
+# the element is <rx.core.observable.groupedobservable.GroupedObservable object at 0x0000022B34BECF88>
+# the element is <rx.core.observable.groupedobservable.GroupedObservable object at 0x0000022B34BECE08>
