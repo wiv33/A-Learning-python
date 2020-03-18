40b344365e1ee8f8770b3d9ff0c301047985a961
PSK <wiv821253@gmail.com>
2020-03-18 21:49:51 +0900

    add scan

---
+++ RxPY/_000_hello_rx_py/_007_transformation_Operators/_003_scan__accumulator.py
@@ -0,0 +1,14 @@
+﻿"""this operator will apply an accumulator function to the values coming
+from the source observable and return an observable with new values.
+
+:parameter
+    accumulator_func: This function is applied to all the values
+    from the source observable.
+
+    seed: [optional] The initial value to be used inside in accumulator_func.
+
+:return
+    This operator will return an observable that will have new values based
+    on the accumulator function applied on each value of the source observable
+"""
+
