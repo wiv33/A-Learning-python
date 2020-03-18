11aedd88b4aea62ee16c66f72622dc351e4ccd5e
PSK <wiv821253@gmail.com>
2020-03-18 21:59:12 +0900

    add scan

--- RxPY/_000_hello_rx_py/_007_transformation_Operators/_003_scan__accumulator.py
+++ RxPY/_000_hello_rx_py/_007_transformation_Operators/_003_scan__accumulator.py
@@ -12,3 +12,20 @@ from the source observable and return an observable with new values.
     on the accumulator function applied on each value of the source observable
 """
 
+from rx import of, operators as op
+
+acc_x = lambda acc, x: acc + x
+of(1,2,3,4,5,6,7,8,90).pipe(
+    op.scan(acc_x, 0)
+).subscribe(lambda x: print("this element is {}".format(x)))
+
+# result
+# this element is 1
+# this element is 3
+# this element is 6
+# this element is 10
+# this element is 15
+# this element is 21
+# this element is 28
+# this element is 36
+# this element is 126
