1f3860e110c54539498c3df79dfc3fb37221b1f6
PSK <wiv821253@gmail.com>
2020-03-20 00:31:25 +0900

    인공신경망 객체 만들 예제 생성

---
+++ machine-learning/_000_hello_machine/_001_books/_002_신경망_첫걸음/_000_DogClass.py
@@ -0,0 +1,34 @@
+﻿class Dog:
+
+    # 초기화
+    def __init__(self, pet_name, temp):
+        self.name = pet_name
+        self.temperature = temp
+
+
+    def status(self):
+        print("dog name is ", self.name)
+        print("dog temperature is ", self.temperature)
+        pass
+
+    def set_temperature(self, temp):
+        self.temperature = temp
+        pass
+
+    def bark(self):
+        print("woof!")
+        pass
+    pass
+
+lassie = Dog("Lassie", 37)
+
+lassie.status()
+# result
+# dog name is  Lassie
+# dog temperature is  37
+
+lassie.set_temperature(15)
+lassie.status()
+# result
+# dog name is  Lassie
+# dog temperature is  15
