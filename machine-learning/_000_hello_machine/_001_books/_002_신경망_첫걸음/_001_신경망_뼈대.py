ffef9296f2aa125e2e42635f362179f3ee52a32b
PSK <wiv821253@gmail.com>
2020-03-20 00:34:04 +0900

    신경망 초기화

---
+++ machine-learning/_000_hello_machine/_001_books/_002_신경망_첫걸음/_001_신경망_뼈대.py
@@ -0,0 +1,16 @@
+﻿"""
+- 초기화: 입력, 은닉, 출력 노드의 수 설정
+- 학습: 학습 데이터들을 통해 학습하고 이에 따라 가중치를 업데이트
+- 질의: 입력을 받아 연산한 후 출력 노드에서 답을 전달
+
+"""
+
+class NeuralNetwork:
+    def __init__(self):
+        pass
+
+    def train(self):
+        pass
+
+    def query(self):
+        pass
