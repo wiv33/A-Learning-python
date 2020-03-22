139d90f36e962775d4a4e85b9608058dd52f5773
PSK <wiv821253@gmail.com>
2020-03-22 23:21:23 +0900

    출력

--- machine-learning/_000_hello_machine/_000_basic/_003_tensorflow_cuk_edu/_03강_MNIST_손글씨_hello_world/_002_drop_out_matplot.py
+++ machine-learning/_000_hello_machine/_000_basic/_003_tensorflow_cuk_edu/_03강_MNIST_손글씨_hello_world/_002_drop_out_matplot.py
@@ -1,27 +1,28 @@
 ﻿import tensorflow as tf
-from tensorflow.examples.tutorials.mnist import input_data
-mnist = input_data.read_data_sets("./mnist/data/", one_hot=True) # one_hot은 하나 빼고 0으로 만들어주는 것
 
-# 28 * 28
-# 모델 변수
+from tensorflow.examples.tutorials.mnist import input_data
+import numpy as np
+import matplotlib.pyplot as plt
+
+mnist = input_data.read_data_sets("./mnist/data/", one_hot=True)
+
 X = tf.placeholder(tf.float32, [None, 784])
 Y = tf.placeholder(tf.float32, [None, 10])
+keep_prob = tf.placeholder(tf.float32)
 
-# 모델 생성
-# ------------------------------------------------------------------------------------------
 # 첫 번째 레이어
-# 256은 뉴런의 개수
 W1 = tf.Variable(tf.random_normal([784, 256], stddev=0.01))
 L1 = tf.nn.relu(tf.matmul(X, W1))  # relu => 활성화 함수, 행렬의 곱
+L1 = tf.nn.dropout(L1, keep_prob)
 
 # 두 번째 레이어
 W2 = tf.Variable(tf.random_normal([256, 256], stddev=0.01))
 L2 = tf.nn.relu(tf.matmul(L1, W2)) # 첫 번째 레이어가 입력 값
+L2 = tf.nn.dropout(L2, keep_prob)
 
 # 세 번째 레이어 -> 출력 레이어
 W3 = tf.Variable(tf.random_normal([256, 10], stddev=0.01))
 model = tf.matmul(L2, W3)
-
 
 
 cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(logits=model, labels=Y))
@@ -30,17 +31,13 @@ optimizer = tf.train.AdamOptimizer(0.001).minimize(cost)
 
 # 학습을 위한 초기화
 # ------------------------------------------------------------------------------------------
-
 init = tf.global_variables_initializer()
-session = tf.Session()
-session.run(init)
+sess = tf.Session()
+sess.run(init)
 
 
-# batch == chunk
 batch_size = 100
 total_batch = int(mnist.train.num_examples / batch_size)
-# ------------------------------------------------------------------------------------------
-
 
 # 학습 부분
 # ------------------------------------------------------------------------------------------
@@ -51,7 +48,10 @@ for epoch in range(15):
     for i in range(total_batch):
         batch_xs, batch_ys = mnist.train.next_batch(batch_size)
 
-        _, cost_val = session.run([optimizer, cost], feed_dict={X: batch_xs, Y: batch_ys})
+        # 학습 시 drop_out 은 80%
+        _, cost_val = sess.run([optimizer, cost], feed_dict={X: batch_xs,
+                                                             Y: batch_ys,
+                                                             keep_prob: 0.8})
         total_cost += cost_val
 
     print("Epoch: ", '%04d' % (epoch + 1), 'Avg. cost = ', '{:.3f}'.format(total_cost / total_batch))
@@ -64,4 +64,22 @@ print("최적화 완료")
 is_correct = tf.equal(tf.argmax(model, 1), tf.argmax(Y, 1)) # 정확도를 위해 계산
 accuracy = tf.reduce_mean(tf.cast(is_correct, tf.float32)) # 정확도를 계산
 
-print('정확도: ', session.run(accuracy, feed_dict={X: mnist.test.images, Y: mnist.test.labels}))
+print('정확도: ', sess.run(accuracy, feed_dict={X: mnist.test.images,
+                                             Y: mnist.test.labels,
+                                             keep_prob: 1}))
+
+labels = sess.run(model, feed_dict={X: mnist.test.images,
+                                             Y: mnist.test.labels,
+                                             keep_prob: 1})
+
+# 결과 값 그리기
+fig = plt.figure()
+for i in range(10):
+    subplot = fig.add_subplot(2, 5, i + 1)
+    subplot.set_xticks([])
+    subplot.set_yticks([])
+    subplot.set_title('%d' % np.argmax(labels[i]))
+    subplot.imshow(mnist.test.images[i].reshape((28, 28)), cmap=plt.cm.gray_r)
+
+plt.show()
+# ------------------------------------------------------------------------------------------
