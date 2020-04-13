7438d216743598cb27ca747e5a91be2d190ecac3
PSK <wiv821253@gmail.com>
2020-04-13 12:31:43 +0900

    정렬 변경

--- machine-learning/_000_hello_machine/_000_basic/_001_Numpy/_017_배열_vector_dot.py
+++ machine-learning/_000_hello_machine/_000_basic/_001_Numpy/_017_배열_vector_dot.py
@@ -11,18 +11,17 @@ y = np.array([[5, 6], [7, 8]])
 v = np.array([9, 10])
 w = np.array([11, 12])
 
-
 # 벡터의 내적:
 # 둘 다 벡터가 들어오면 내적
 v.dot(w)
-np.dot(v,w)
+np.dot(v, w)
 
 # 행렬과 벡터의 곱
 # dimension 1인 배열 [29 67]
 x.dot(v)
-np.dot(x,v)
+np.dot(x, v)
 
 # 행렬 곱
 # dimension 2인 배열
 x.dot(y)
-np.dot(x,y)
+np.dot(x, y)
