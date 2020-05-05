ec7b049ce4f1f99c823f62ffd984e3d6fff30e81
ps <wiv821253@gmail.com>
2020-05-05 19:56:04 +0900

    add python -v

--- testPython/2588.py
+++ testPython/2588.py
@@ -1,7 +1,15 @@
 a = 472
 b = 385
 
+
 def getNum(n, digit):
     digit_num = pow(10, digit)
+    div, mod = divmod(n, digit_num)
+    return div % 10
+
+
+assert getNum(a, 2) == 4
+assert getNum(a, 1) == 7
+assert getNum(a, 0) == 2
 
 
