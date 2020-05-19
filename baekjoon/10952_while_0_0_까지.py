49d8a6f935ad44e2079786d8fcd7fac6d721d777
PSK <wiv821253@gmail.com>
2020-05-20 01:31:02 +0900

    정렬

--- baekjoon/10952_while_0_0_까지.py
+++ baekjoon/10952_while_0_0_까지.py
@@ -1,7 +1,7 @@
 ﻿from sys import stdin
 
 while True:
-    a, b = map(int , stdin.readline().rstrip().split(" "))
+    a, b = map(int, stdin.readline().rstrip().split(" "))
     if a == 0 and b == 0:
         break
 
