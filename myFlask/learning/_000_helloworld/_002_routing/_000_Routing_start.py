7cff0acee3d0a2987ee18ed903e332a1795ad4c5
PSK <40161102+wiv33@users.noreply.github.com>
2020-06-10 20:57:26 +0900

    indent 정리

--- myFlask/learning/_000_helloworld/_002_routing/_000_Routing_start.py
+++ myFlask/learning/_000_helloworld/_002_routing/_000_Routing_start.py
@@ -4,25 +4,30 @@ app = Flask(__name__)
 
 app.debug = True
 
-myDictionary = {"my_name":"pil_seong", "skill":"hot"}
+myDictionary = {"my_name": "pil_seong", "skill": "hot"}
+
 
 @app.route("/")
 def hello_world():
     return "Hello World BODY!!"
+
 
 @app.route("/board", methods=['GET'])
 def board_list():
     print("board_list")
     return make_response(('flask framework', 200, {"program": "language python"}))
 
+
 @app.route("/board", methods=['POST'])
 def post_board_list():
     return make_response("POST BODY")
+
 
 # PathVariable <name>
 @app.route('/board/<name>')
 def end_point_board_list(name):
     return make_response("end_point=boarder, get Name {0}".format(name))
+
 
 with app.test_request_context('/'):
     print("run ! test_request_context")
