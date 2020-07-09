e0ab44c7654b1f8aad9af8a74642453bab89f230
PSK <wiv821253@gmail.com>
2020-07-09 10:04:26 +0900

    article 형태소 분석

--- machine-learning/_000_hello_machine/_000_basic/_004_multi_camp/_004_텍스트_마이닝.py
+++ machine-learning/_000_hello_machine/_000_basic/_004_multi_camp/_004_텍스트_마이닝.py
@@ -1,14 +1,31 @@
 import codecs
+import rx
+from rx import operators as ops
 from konlpy.tag import Komoran, Twitter
 
+
+def print_word(word, filter_word):
+    rx.from_(word).pipe(
+        ops.filter(lambda tup: tup[1] == filter_word and len(tup[0]) > 1),
+        ops.map(lambda tup: tup[0])
+    ).subscribe(lambda x: print("word is {}".format(x)))
+
+
+file = open(
+    "C:\\Users\\user\\PycharmProjects\\"
+    "learning-python\\machine-learning\\_000_hello_machine\\_000_basic\\_004_multi_camp\\article.txt", mode='r',
+    encoding='utf-8')
+
+article = file.read()
+
 model = Komoran()
+komoran_pos = model.pos(article)
 
-line = '나는 국민대학교의 축제에 놀러 가고 싶습니다.'
-
-result = model.pos(line)
-
-print(result)
+print_word(komoran_pos, "NNP")
 
 twitter = Twitter()
+twitter_pos = twitter.pos(article)
 
-print(twitter.pos(line))
+print(twitter_pos)
+
+print_word(twitter_pos, "Noun")
