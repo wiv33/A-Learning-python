689291d17aa381aa1db7b96f0d48e6e0626a0c82
ps <wiv821253@gmail.com>
2020-07-30 01:01:08 +0900

    nomessage

--- practice_algorithm/study_leet_code/_001_two_numbers.py
+++ practice_algorithm/study_leet_code/_001_two_numbers.py
@@ -9,7 +9,8 @@ class Solution:
         arr = []
         temp1, temp2 = l1, l2
         n_val = 0
-        while l1 is not None or l2 is not None or n_val is not 0:
+        v = 0;
+        while l1 is not None or l2 is not None:
             a, b = 0, 0
             if hasattr(l1, "val"):
                 a = l1.val
@@ -26,19 +27,15 @@ class Solution:
             # 몫이 있으면 n + 1 요소는 증가, n은 v/10 + m
             d, m = divmod(v, 10)
             print(d, m)
-            if d > 0:
+            if d > 0 and n_val == 0:
                 arr.append(m)
-                if 0 >= n_val:
-                    n_val = 1
-                else:
-                    n_val = 0
+                n_val = 1
+            elif n_val == 1:
+                arr.append(m)
+                n_val = 0
             else:
                 print('v = {}'.format(v))
                 arr.append(v)
-                n_val = 0
-
-            # v + d >= 10: n + 1 요소에 1을 증가시킨다.
-            v + d
 
         return eval(generate_node(arr))
 
