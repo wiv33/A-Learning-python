78789e8773580322181c33af2f6bb3aa50d4ddef
ps <wiv821253@gmail.com>
2020-07-30 00:45:26 +0900

    변경

--- practice_algorithm/study_leet_code/_001_two_numbers.py
+++ practice_algorithm/study_leet_code/_001_two_numbers.py
@@ -1,4 +1,3 @@
-# https://leetcode.com/problems/add-two-numbers/submissions/
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val=0, next=None):
@@ -7,45 +6,41 @@
 class Solution:
     def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
 
-        val_arr = []
+        arr = []
         temp1, temp2 = l1, l2
-        # first_l2 = l2
-        increment = 0
-        unbal: bool = False
-
-        while l1 is not None or l2 is not None:
-            if unbal and len(val_arr) == 1:
-                if l1 is None:
-                    return temp2
-                elif l2 is None:
-                    return temp1
-
+        n_val = 0
+        while l1 is not None or l2 is not None or n_val is not 0:
             a, b = 0, 0
             if hasattr(l1, "val"):
                 a = l1.val
             if hasattr(l2, "val"):
                 b = l2.val
 
-            v = a + b
+            if hasattr(l1, "next"):
+                l1 = l1.next
+            if hasattr(l2, "next"):
+                l2 = l2.next
+
+            # 현재 값
+            v = a + b + n_val
+            # 몫이 있으면 n + 1 요소는 증가, n은 v/10 + m
             d, m = divmod(v, 10)
+            print(d, m)
             if d > 0:
-                v = v - v + m
-                increment = d
-            elif increment > 0:
-                v += increment
-                increment = 0
+                arr.append(m)
+                if 0 >= n_val:
+                    n_val = 1
+                else:
+                    n_val = 0
+            else:
+                print('v = {}'.format(v))
+                arr.append(v)
+                n_val = 0
 
-            val_arr.append(v)
-            if increment is not 0 and l2.next is None:
-                val_arr.append(d)
+            # v + d >= 10: n + 1 요소에 1을 증가시킨다.
+            v + d
 
-            l1 = l1.next
-            l2 = l2.next
-
-            if l1 is None or l2 is None:
-                unbal = True
-
-        return eval(generate_node(val_arr))
+        return eval(generate_node(arr))
 
 
 def generate_node(arr: list):
