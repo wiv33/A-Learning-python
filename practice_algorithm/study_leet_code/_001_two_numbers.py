99f2dc8ecf76e773f9eafd676477140130cf0dba
PSK <wiv821253@gmail.com>
2020-07-30 17:25:20 +0900

    reset

--- practice_algorithm/study_leet_code/_001_two_numbers.py
+++ practice_algorithm/study_leet_code/_001_two_numbers.py
@@ -5,39 +5,7 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
-
-        arr = []
-        temp1, temp2 = l1, l2
-        n_val = 0
-        v = 0;
-        while l1 is not None or l2 is not None:
-            a, b = 0, 0
-            if hasattr(l1, "val"):
-                a = l1.val
-            if hasattr(l2, "val"):
-                b = l2.val
-
-            if hasattr(l1, "next"):
-                l1 = l1.next
-            if hasattr(l2, "next"):
-                l2 = l2.next
-
-            # 현재 값
-            v = a + b + n_val
-            # 몫이 있으면 n + 1 요소는 증가, n은 v/10 + m
-            d, m = divmod(v, 10)
-            print(d, m)
-            if d > 0 and n_val == 0:
-                arr.append(m)
-                n_val = 1
-            elif n_val == 1:
-                arr.append(m)
-                n_val = 0
-            else:
-                print('v = {}'.format(v))
-                arr.append(v)
-
-        return eval(generate_node(arr))
+        pass
 
 
 def generate_node(arr: list):
