8c88c2676f2606846c2f8e28a6db4a77a3e9e57e
PSK <wiv821253@gmail.com>
2020-07-30 17:58:33 +0900

    add

--- practice_algorithm/study_leet_code/_001_two_numbers.py
+++ practice_algorithm/study_leet_code/_001_two_numbers.py
@@ -1,3 +1,4 @@
+# https://leetcode.com/problems/add-two-numbers/
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val=0, next=None):
@@ -5,12 +6,30 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
-        pass
+        increment = 0
 
+        a = l1.val if l1 else 0
+        b = l2.val if l2 else 0
+        val = a + b + increment
+        increment, m = divmod(val, 10)
 
-def generate_node(arr: list):
-    result = ""
-    for v in arr:
-        result += "ListNode({},".format(v)
-    result += (")" * len(arr))
-    return result
+        first = ListNode(m)
+        l1 = l1.next if hasattr(l1, "next") else None
+        l2 = l2.next if hasattr(l2, "next") else None
+        while l1 or l2 or increment:
+            a = l1.val if l1 else 0
+            b = l2.val if l2 else 0
+
+            val = a + b + increment
+            increment, m = divmod(val, 10)
+
+            first.next = ListNode(m)
+
+            l1 = l1.next if hasattr(l1, "next") else None
+            l2 = l2.next if hasattr(l2, "next") else None
+
+        if increment > 0:
+            first.next = ListNode(increment)
+
+        return first
+
