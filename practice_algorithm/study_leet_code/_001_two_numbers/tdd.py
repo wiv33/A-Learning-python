d2497c7fd25142bb9d6d4d13094bcd0b241aff5c
ps <wiv821253@gmail.com>
2020-07-30 23:16:59 +0900

    add test

--- practice_algorithm/study_leet_code/_001_two_numbers/tdd.py
+++ practice_algorithm/study_leet_code/_001_two_numbers/tdd.py
@@ -12,12 +12,24 @@ class MyTestCase(unittest.TestCase):
     def test_import(self):
         self.assertIsNotNone(self.l1)
         self.assertIsNotNone(self.l2)
-        self.assertEqual(self.l1.val, 2, "l1.val is 2")
-        self.assertEqual(self.l2.val, 5, "l2.val is 5")
-        self.assertEqual(self.l1.next.val, 4, "l1.next.val is 4")
-        self.assertEqual(self.l2.next.val, 6, "l2.next.val is 6")
-        self.assertEqual(self.l1.next.next.val, 3, "l1.next.next.val is 3")
-        self.assertEqual(self.l2.next.next.val, 4, "l2.next.next.val is 4")
+        self.assertEqual(2, self.l1.val, "l1.val is 2")
+        self.assertEqual(5, self.l2.val, "l2.val is 5")
+        self.assertEqual(4, self.l1.next.val, "l1.next.val is 4")
+        self.assertEqual(6, self.l2.next.val, "l2.next.val is 6")
+        self.assertEqual(3, self.l1.next.next.val, "l1.next.next.val is 3")
+        self.assertEqual(4, self.l2.next.next.val, "l2.next.next.val is 4")
+
+    def tearDown(self) -> None:
+        self.l1, self.l2, self.expected = None, None, None
+
+    def test_calculate(self):
+        self.assertEqual(7, self.l1.val + self.l2.val, "expected 7, actual 7")
+        d, m = divmod(10, 10)
+        self.assertEqual(d, 1, "div is 1")
+        self.assertEqual(m, 0, "mod is 0")
+
+    def test_node_chain(self):
+        pass
 
 
 if __name__ == '__main__':
