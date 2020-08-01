e47fb0b5109dd8ae7e74e44de3d2e09b7cebba98
ps <wiv821253@gmail.com>
2020-08-01 19:18:21 +0900

    update max_index

--- practice_algorithm/모두의_알고리즘/_001_list/find_max.py
+++ practice_algorithm/모두의_알고리즘/_001_list/find_max.py
@@ -6,3 +6,12 @@ def find_max(arr: []) -> int:
             max_val = arr[i]
 
     return max_val
+
+
+def find_max_index(arr: []) -> int:
+    max_index = 0
+    for i in range(1, len(arr)):
+        if arr[i] > arr[max_index]:
+            max_index = i
+
+    return max_index
