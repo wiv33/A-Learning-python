6799a85f7347075deead08dbdd22ff1ec26246b9
ps <wiv821253@gmail.com>
2020-08-02 01:18:54 +0900

    add fibonacci

--- practice_algorithm/모두의_알고리즘/_004_gcd/test_my_gcd.py
+++ practice_algorithm/모두의_알고리즘/_004_gcd/test_my_gcd.py
@@ -1,5 +1,6 @@
 from unittest import TestCase
 
+from fibonacci import fibonacci
 from my_gcd import gcd
 
 
@@ -7,3 +8,6 @@ class Test(TestCase):
     def test_gcd(self):
         self.assertEqual(2, gcd(4, 6))
         self.assertEqual(3, gcd(6, 9))
+
+    def test_fibonacci(self):
+        self.assertEqual(21, fibonacci(8, "init"))
