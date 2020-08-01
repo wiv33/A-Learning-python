8b2db0546b6a6eda5707c7649b6502048a311c06
ps <wiv821253@gmail.com>
2020-08-01 20:12:26 +0900

    add test

--- practice_algorithm/모두의_알고리즘/_001_set/same_name_test.py
+++ practice_algorithm/모두의_알고리즘/_001_set/same_name_test.py
@@ -1,9 +1,18 @@
 import unittest
+
 from .same_name import find_same_name
 
 
 class MySameNameTest(unittest.TestCase):
+    def setUp(self) -> None:
+        self.names = ['Tomcat', "Jetty", "Netty", "Undertow", "Netty"]
+
     def test_find_same_name(self):
-        names = ['Tomcat', "Jetty", "Netty", "Undertow", "Netty"]
-        name_set = find_same_name(names)
+        name_set = find_same_name(self.names)
         self.assertEqual({"Netty"}, name_set)
+
+    def test_find_same_pop(self):
+        self.names.pop(len(self.names) - 1)
+        name = find_same_name(self.names)
+        self.assertEqual(set(), name)
+        self.assertTrue(len(name) == 0)
