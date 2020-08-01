71996c454c3f347a9a46188926d80d731569e458
ps <wiv821253@gmail.com>
2020-08-01 19:12:02 +0900

    max val index

--- practice_algorithm/모두의_알고리즘/_001_list/find_max_test.py
+++ practice_algorithm/모두의_알고리즘/_001_list/find_max_test.py
@@ -8,5 +8,9 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(65, find_max([5, 6, 3, 6, 1, 2, 5, 7, 8, 10, 52, 65, 45]))
         self.assertEqual(33, find_max([33, 5, 6, 3, 6, 1, 2, 5, 7, 8, 10, ]))
 
+    def test_find_max_index(self):
+        self.assertEqual(33, find_max_index([33, 5, 6, 3, 6, 1, 2, 5, 7, 8, 10, ]))
+
+
     if __name__ == '__main__':
         unittest.main()
