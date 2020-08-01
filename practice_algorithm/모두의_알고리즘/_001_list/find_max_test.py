73d12349e79adf474edc35401305d5979796b35b
ps <wiv821253@gmail.com>
2020-08-01 19:18:21 +0900

    update max_index

--- practice_algorithm/모두의_알고리즘/_001_list/find_max_test.py
+++ practice_algorithm/모두의_알고리즘/_001_list/find_max_test.py
@@ -1,6 +1,6 @@
 import unittest
 
-from .find_max import find_max
+from .find_max import find_max, find_max_index
 
 
 class MyTestCase(unittest.TestCase):
@@ -9,8 +9,7 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(33, find_max([33, 5, 6, 3, 6, 1, 2, 5, 7, 8, 10, ]))
 
     def test_find_max_index(self):
-        self.assertEqual(33, find_max_index([33, 5, 6, 3, 6, 1, 2, 5, 7, 8, 10, ]))
-
+        self.assertEqual(2, find_max_index([17, 24, 33, 5, 6, 3, 6, 1, 2, 5, 7, 8, 10, ]))
 
     if __name__ == '__main__':
         unittest.main()
