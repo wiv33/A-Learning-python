771cb389d422c9aa5bb3cb4f4123931be4511422
ps <wiv821253@gmail.com>
2020-08-01 20:56:11 +0900

    add comment

--- practice_algorithm/모두의_알고리즘/_003_factorial/my_factorial.py
+++ practice_algorithm/모두의_알고리즘/_003_factorial/my_factorial.py
@@ -1,5 +1,6 @@
 def factorial(n: int) -> int:
     if n <= 1:
         return n
-
-    return factorial(n - 1) * n
+    # 4 -> factorial(4-1): 3 -> factorial(3-1): 2 -> factorial(2-1): 1
+    # 4 * 3 * 2 * 1
+    return n * factorial(n - 1)
