eaa6f13f47d864bb4a3023c719463b3af118b8da
ps <wiv821253@gmail.com>
2020-08-01 23:40:58 +0900

    add 최댓값 찾기

--- practice_algorithm/모두의_알고리즘/_003_factorial/recursion.py
+++ practice_algorithm/모두의_알고리즘/_003_factorial/recursion.py
@@ -1,10 +1,19 @@
 def recursion_sum(n: int) -> int:
     if n < 1:
         return n
+
     return n + recursion_sum(n - 1)
 
 
 def recursion_max_val(arr: []):
-    if len(arr) == 1:
+    length = len(arr)
+    if length < 2:
         return arr[0]
-    return recursion_max_val([max_val for max_val in arr if arr[0] < max_val])
+
+    result = [arr[0]]
+    for x in range(1, length - 1):
+        val = arr.pop()
+        if result[0] < val:
+            result[0] = val
+
+    return recursion_max_val(result)
--- practice_algorithm/모두의_알고리즘/_003_factorial/test_recursion.py
+++ practice_algorithm/모두의_알고리즘/_003_factorial/test_recursion.py
@@ -11,4 +11,5 @@ class Test(TestCase):
 class TestMaxVal(TestCase):
     def test_recursion_max_val(self):
         self.assertEqual(10, recursion_max_val([2, 3, 4, 9, 10, 7, 6, 7]))
+        self.assertEqual(9, recursion_max_val([2, 3, 4, 9, 1, 7, 6, 7]))
 
