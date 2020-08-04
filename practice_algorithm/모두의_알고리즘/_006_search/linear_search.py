a653c2d2bbe738734b64a0a90afeb0371d7a01e5
ps <wiv821253@gmail.com>
2020-08-05 00:42:46 +0900

    add init

---
+++ practice_algorithm/모두의_알고리즘/_006_search/linear_search.py
@@ -0,0 +1,10 @@
+def search_list(a: [], target: int):
+    for n in range(len(a)):
+        if a[n] == target:
+            return n
+
+    return -1
+
+
+def search_arr(a: [], target: int):
+   pass
