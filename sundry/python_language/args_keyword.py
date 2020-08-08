6c606e5815eebe032f9c1fcc529bd5b8b7410500
ps <wiv821253@gmail.com>
2020-08-08 19:19:25 +0900

    add comment

--- sundry/python_language/args_keyword.py
+++ sundry/python_language/args_keyword.py
@@ -5,3 +5,13 @@ def print_args(farg, *args):
 
 
 print_args(1, "two", 3, True, "what", 7, 8, [], {}, str(("a", "b")))
+# formal arg: 1
+# another positional arg: two
+# another positional arg: 3
+# another positional arg: True
+# another positional arg: what
+# another positional arg: 7
+# another positional arg: 8
+# another positional arg: []
+# another positional arg: {}
+# another positional arg: ('a,', 'b')
