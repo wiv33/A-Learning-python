38421a4ebdf2d2359b88a793b5d51514ae51fe3e
ps <wiv821253@gmail.com>
2020-08-08 18:31:51 +0900

    add 코드정리

--- practice_algorithm/study_leet_code/_002_length_of_longest_substring/solution.py
+++ practice_algorithm/study_leet_code/_002_length_of_longest_substring/solution.py
@@ -1,6 +1,64 @@
 def length_of_longest_substring(s: str) -> int:
+    return get_first_pattern(s)
 
-    return 3
+
+def get_first_pattern(s: str) -> int:
+    length = len(s)
+
+    if length == 0:
+        return 0
+
+    acc = s[0] or ""
+
+    if acc * len(s) == s:
+        return 1
+
+    if is_high_pass_not_match(s):
+        return len(s)
+
+    dictionary = dict()
+
+    for i in range(length - 1):
+        l1, l2 = s[i], s[i + 1]
+
+        if acc.__contains__(l2):
+            add_dict(dictionary, acc)
+            acc = acc[acc.index(l2) + 1:]
+
+        acc += l2
+
+    add_dict(dictionary, acc)
+    return result_int(dictionary)
+
+
+def add_dict(dictionary, acc):
+    dictionary.__setitem__(acc, dictionary.get(acc) if acc in dictionary else 1)
+    # print(dictionary)
+
+
+def result_int(dictionary: {}) -> int:
+    if len(dictionary) == 1:
+        return len(dictionary.popitem()[0])
+
+    max_item = dictionary.popitem()
+    for i in sorted(dictionary.items(),
+                    key=lambda x: x[1],
+                    reverse=True):
+        if max_item[1] > i[1]:
+            return max_item
+
+        if max_item[1] < i[1]:
+            max_item = i
+        elif max_item[1] == i[1] and len(max_item[0]) < len(i[0]):
+            max_item = i
+
+    return len(max_item[0])
+
+
+def is_high_pass_not_match(s):
+    for z in range(1, len(s)):
+        if s[z - 1] == s[z]:
+            return False
 
 
 class Solution:
--- practice_algorithm/study_leet_code/_002_length_of_longest_substring/tdd.py
+++ practice_algorithm/study_leet_code/_002_length_of_longest_substring/tdd.py
@@ -1,5 +1,7 @@
 import unittest
 
+from .solution import get_first_pattern, length_of_longest_substring
+
 
 class MyTestCase(unittest.TestCase):
 
@@ -7,18 +9,17 @@ class MyTestCase(unittest.TestCase):
         self.first_input = "abcabcbb"
 
     def test_final(self):
-        pass
-        # actual = Solution().lengthOfLongestSubstring("abcabcbb")
-        # expected = 3
-        # self.assertEqual(expected, actual)
-        #
-        # actual = Solution().lengthOfLongestSubstring("bbbbb")
-        # expected = 1
-        # self.assertEqual(expected, 1)
-        #
-        # actual = Solution().lengthOfLongestSubstring("pwwkew")
-        # expected = 3
-        # self.assertEqual(expected, 3)
+        actual = length_of_longest_substring("abcabcbb")
+        expected = 3
+        self.assertEqual(expected, actual)
+
+        actual = length_of_longest_substring("bbbbb")
+        expected = 1
+        self.assertEqual(expected, actual)
+
+        actual = length_of_longest_substring("pwwkew")
+        expected = 3
+        self.assertEqual(expected, actual)
 
     # 덩어리로 확인할 수 없으니 문자열을 쪼개야 한다.
     # python은 문자열을 이미 배열로 가지고 있다.
@@ -100,65 +101,6 @@ class MyTestCase(unittest.TestCase):
             print(ss[0], ss[1])
 
         print(type(ss))
-
-
-def get_first_pattern(s: str) -> int:
-    length = len(s)
-
-    if length == 0:
-        return 0
-
-    acc = s[0] or ""
-
-    if acc * len(s) == s:
-        return 1
-
-    if is_high_pass_not_match(s):
-        return len(s)
-
-    dictionary = dict()
-
-    for i in range(length - 1):
-        l1, l2 = s[i], s[i + 1]
-
-        if acc.__contains__(l2):
-            add_dict(dictionary, acc)
-            acc = acc[acc.index(l2) + 1:]
-
-        acc += l2
-
-    add_dict(dictionary, acc)
-    return result_int(dictionary)
-
-
-def add_dict(dictionary, acc):
-    dictionary.__setitem__(acc, dictionary.get(acc) if acc in dictionary else 1)
-    # print(dictionary)
-
-
-def result_int(dictionary: {}) -> int:
-    if len(dictionary) == 1:
-        return len(dictionary.popitem()[0])
-
-    max_item = dictionary.popitem()
-    for i in sorted(dictionary.items(),
-                    key=lambda x: x[1],
-                    reverse=True):
-        if max_item[1] > i[1]:
-            return max_item
-
-        if max_item[1] < i[1]:
-            max_item = i
-        elif max_item[1] == i[1] and len(max_item[0]) < len(i[0]):
-            max_item = i
-
-    return len(max_item[0])
-
-
-def is_high_pass_not_match(s):
-    for z in range(1, len(s)):
-        if s[z - 1] == s[z]:
-            return False
 
 
 if __name__ == '__main__':
