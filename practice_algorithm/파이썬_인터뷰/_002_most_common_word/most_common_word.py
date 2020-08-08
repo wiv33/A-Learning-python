cbf03f129d73f4222f144ae7d12722149bdfab29
ps <wiv821253@gmail.com>
2020-08-08 19:19:03 +0900

    add language spec

--- practice_algorithm/파이썬_인터뷰/_002_most_common_word/most_common_word.py
+++ practice_algorithm/파이썬_인터뷰/_002_most_common_word/most_common_word.py
@@ -1,6 +1,23 @@
+import collections
+import re
+
+
 class Solution:
     def __init__(self, input_data, banned):
-        pass
+        self.data = input_data
+        self.banned = banned
 
     def most_common_word(self):
-        pass
+        words = [word for word in re.sub(r'[^\w]', ' ', self.data).lower().split() if word not in self.banned]
+        counts = collections.Counter(words)
+        for word in words:
+            counts[word] += 1
+
+        common = counts.most_common(1)
+        # print(common)
+        # [('ball', 4)]
+        assert type(common) == list
+        assert type(common[0]) == tuple
+        assert type(common[0][0]) == str
+
+        return common[0][0]
