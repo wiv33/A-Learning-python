d60b571f9cc2946f5d1a4507b788bc50e0294394
ps <wiv821253@gmail.com>
2020-08-08 20:59:50 +0900

    add group_anagrams.py

--- practice_algorithm/파이썬_인터뷰/_003_group_anagrams/group_anagrams.py
+++ practice_algorithm/파이썬_인터뷰/_003_group_anagrams/group_anagrams.py
@@ -1,6 +1,15 @@
+import collections
+
+
 class Solution:
     def __init__(self, data: []):
         self.data = data
 
     def group_anagrams(self):
-        pass
+        anagrams = collections.defaultdict(list)
+
+        for word in self.data:
+            anagrams[''.join(sorted(word))].append(word)
+
+        return anagrams
+
