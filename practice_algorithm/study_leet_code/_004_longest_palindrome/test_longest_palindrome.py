582d3544167503335c4decc7f425ff11fce2e332
ps <wiv821253@gmail.com>
2020-08-10 00:42:35 +0900

    add other solution

--- practice_algorithm/study_leet_code/_004_longest_palindrome/test_longest_palindrome.py
+++ practice_algorithm/study_leet_code/_004_longest_palindrome/test_longest_palindrome.py
@@ -1,9 +1,16 @@
 from unittest import TestCase
 
 from .longest_palindrome import Solution
+from .solution_ import SolutionOther
 
 
 class TestSolution(TestCase):
     def test_longest_palindrome(self):
         palindrome = Solution("babad").longest_palindrome()
         self.assertEqual("bab", palindrome)
+
+    def test_longest_other(self):
+        actual = SolutionOther().sol("babad")
+        self.assertEqual("bab", actual)
+
+        SolutionOther().sol("ddaba")
