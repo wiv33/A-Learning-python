7d4bcdb1195d1818ba7c201bdb142150b5d3c9bb
ps <wiv821253@gmail.com>
2020-08-12 21:33:58 +0900

    add expected

--- practice_algorithm/study_leet_code/_005_zigzag-conversion/test_zigzag_conversion.py
+++ practice_algorithm/study_leet_code/_005_zigzag-conversion/test_zigzag_conversion.py
@@ -1,6 +1,13 @@
 from unittest import TestCase
 
+from .zigzag_conversion import Solution
+
 
 class TestSolution(TestCase):
+    def setUp(self) -> None:
+        self.input = (3, "PAYPALISHIRING")
+        self.expected = "PAHNAPLSIIGYIR"
+
     def test_convert(self):
-        self.fail()
+        actual = Solution().convert(s=self.input[1], numRows=3)
+        self.assertEqual(self.expected, actual)
