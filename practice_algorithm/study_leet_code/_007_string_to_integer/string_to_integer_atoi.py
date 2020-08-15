3c69f39a3b9b3f2dc6ad0f6085943fbcf036003d
ps <wiv821253@gmail.com>
2020-08-16 02:31:09 +0900

    what the...

--- practice_algorithm/study_leet_code/_007_string_to_integer/string_to_integer_atoi.py
+++ practice_algorithm/study_leet_code/_007_string_to_integer/string_to_integer_atoi.py
@@ -2,6 +2,12 @@
 import re
 
 
+def return_zero(data):
+    search = re.search(r"[-+[^a-zA-Z]]|[a-zA-Z]|[-+]{2}", data)
+    print("search is {}".format(search))
+    return search
+
+
 class Solution:
     def __init__(self, s: str):
         self.data = s
@@ -9,15 +15,21 @@ class Solution:
     def string_to_integer_atoi(self) -> int:
         s = self.data
         not_empty = s.lstrip()
-        if not not_empty:
+        if not not_empty or not_empty == "+" or not_empty == "-":
             return 0
 
         data = not_empty.split()[0].lstrip()
 
-        if data.isalpha():
+        if return_zero(data):
             return 0
 
-        data = eval(data)
+        data = re.sub(r'(?<=[-+])0*[^1-9]', "", data)
+        print(data)
+        data = re.sub(r'0*$[^1-9]', "", data)
+        print(data)
+        data = re.sub(r'^0*', "", data)
+        print(data)
+        data = eval(data or "0")
 
         data = int(data // 1)
 
--- practice_algorithm/study_leet_code/_007_string_to_integer/test_string_to_integer_atoi.py
+++ practice_algorithm/study_leet_code/_007_string_to_integer/test_string_to_integer_atoi.py
@@ -56,3 +56,67 @@ class Test(TestCase):
         s = "fwokejo"
         self.assertTrue(s.isalpha())
         self.assertTrue("dfef212412".isalnum())
+
+    def test_print_re(self):
+        s = "-+23412"
+        search = re.search(r'([-+]{2})|[a-zA-Z]', s)
+        print(search)
+        self.assertTrue(search)
+
+    def test_available_eval(self):
+        s = "+00000000032342"
+        search = re.search(r'[-+]', s)
+        print(search)
+        self.assertTrue(search)
+
+        s = "-028328032"
+        search = re.search(r'[-+]', s)
+        print(search)
+        self.assertTrue(search)
+
+        s = "  +-028328032"
+        search = re.search(r'[-+[^a-zA-Z]]', s)
+        print(search)
+        self.assertFalse(search)
+
+    def test_re_sub_remove_zero(self):
+        s = "-0000000000012345600000"
+        sub = re.sub(r'(?<=[-+])0*[^1-9]', "", s)
+        self.assertEqual("-12345600000", sub)
+
+        s = "+000000000001234560"
+        sub = re.sub(r'(?<=[-+])0*[^1-9]', "", s)
+        self.assertEqual("+1234560", sub)
+
+    def test_words(self):
+        atoi = Solution("words and 987").string_to_integer_atoi()
+        print(atoi)
+
+    def test_000000000000(self):
+        actual = Solution("    0000000000000   ").string_to_integer_atoi()
+        print(actual)
+
+    def test_eval_0000000000(self):
+        s = "   -42"
+        actual = Solution(s).string_to_integer_atoi()
+        self.assertEqual(-42, actual)
+
+    def test_eval_plus_and_minus(self):
+        s = "+-2"
+        actual = Solution(s).string_to_integer_atoi()
+        self.assertEqual(0, actual)
+
+    def test_eval_010(self):
+        s = "010"
+        actual = Solution(s).string_to_integer_atoi()
+        self.assertEqual(10, actual)
+
+    def test_eval_004500(self):
+        s = "     +004500"
+        actual = Solution(s).string_to_integer_atoi()
+        self.assertEqual(4500, actual)
+
+    def test_eval_what_the(self):
+        s = "  -0012a42"
+        actual = Solution(s).string_to_integer_atoi()
+        self.assertEqual(-12, actual)
