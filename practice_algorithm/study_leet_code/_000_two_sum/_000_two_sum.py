75f5d34d5b8b688c83ba7a301a8a4407031f7fd9
ps <wiv821253@gmail.com>
2020-08-16 17:43:31 +0900

    add new version

--- practice_algorithm/study_leet_code/_000_two_sum/_000_two_sum.py
+++ practice_algorithm/study_leet_code/_000_two_sum/_000_two_sum.py
@@ -10,3 +10,27 @@ class Solution:
 
                 if target == nums[i] + nums[j]:
                     return [i, j]
+
+    def two_sum(self, nums: [int], target: int) -> [int]:
+        for i, n in enumerate(nums):
+            complement = target - n
+
+            if complement in nums[i + 1:]:
+                return nums.index(n), nums[i + 1:].index(complement) + (i + 1)
+
+    def other_two_sum(self, nums: [int], target: int) -> [int]:
+        nums_map = {}
+        for i, num in enumerate(nums):
+            nums_map[num] = i
+
+        for i, num in enumerate(nums):
+            if target - num in nums_map and i != nums_map[target - num]:
+                return nums.index(num), nums_map[target - num]
+
+
+solution = Solution()
+two_sum = solution.other_two_sum([1, 5, 7, 8, 9], 10)
+print((0, 4) == two_sum)
+
+solution_two_sum = solution.two_sum([1, 5, 7, 8, 9], 10)
+print((0, 4) == solution_two_sum)
