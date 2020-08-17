409679e28be3c91d56f08966e8fd3aaddbad175c
ps <wiv821253@gmail.com>
2020-08-17 20:02:47 +0900

    add

--- practice_algorithm/study_leet_code/_008_palindrome_number/test_palindrome_number.py
+++ practice_algorithm/study_leet_code/_008_palindrome_number/test_palindrome_number.py
@@ -1,6 +1,17 @@
 from unittest import TestCase
 
+from .palindrome_number import Solution
+
 
 class TestSolution(TestCase):
     def test_palindrome_number(self):
-        self.fail()
+        self.assertTrue(Solution().isPalindrome(12121))
+
+    def test_palindrome_10101(self):
+        self.assertTrue(Solution().isPalindrome(10101))
+
+    def test_palindrome_number_0(self):
+        self.assertFalse(Solution().isPalindrome(1213))
+
+    def test_palindrome_number_22(self):
+        self.assertTrue(Solution().isPalindrome(22))
---
+++ practice_algorithm/파이썬_인터뷰/_008_trapping_rain_water/trap.py
@@ -0,0 +1,42 @@
+class Solution:
+    def trap(self, height: [int]) -> int:
+        if not height:
+            return 0
+
+        volume = 0
+
+        left, right = 0, len(height) - 1
+        left_max, right_max = height[left], height[right]
+
+        while left < right:
+            left_max, right_max = max(height[left], left_max), max(height[right], right_max)
+
+            if left_max <= right_max:
+                volume += left_max - height[left]
+                left += 1
+            else:
+                volume += right_max - height[right]
+                right -= 1
+
+        return volume
+
+    def trap_stack(self, height: [int]) -> int:
+        stack = []
+        volume = 0
+
+        for i in range(len(height)):
+            # 변곡점을 만나는 경우
+            while stack and height[i] > height[stack[-1]]:
+                top = stack.pop()
+
+                if not len(stack):
+                    break
+
+                # 이전과의 차이만큼 물 높이 처리
+                distance = i - stack[-1] - 1
+                waters = min(height[i], height[stack[-1]]) - height[top]
+
+                volume += distance * waters
+
+            stack.append(i)
+        return volume
