114fb3a669aaec4fdb7b4d299d757a405c69f093
ps <wiv821253@gmail.com>
2020-08-23 14:40:30 +0900

    init iris

---
+++ machine-learning/_000_hello_machine/_000_basic/_005_learn_evening/_002_boston.py
@@ -0,0 +1,18 @@
+import tensorflow as tf
+import pandas as pd
+
+file_data = "https://raw.githubusercontent.com/blackdew/tensorflow1/master/csv/boston.csv"
+data = pd.read_csv(file_data)
+
+독립 =  data[['crim','zn','indus','chas','nox','rm','age','dis','rad','tax','ptratio','b','lstat']]
+종속 = data[['medv']]
+
+X = tf.keras.layers.Input(shape=[13])
+Y = tf.keras.layers.Dense(1)(X)
+
+model = tf.keras.models.Model(X, Y)
+
+model.compile(loss='mse')
+model.fit(독립, 종속, epochs=10000, verbose=0)
+
+print(model.weights)
---
+++ machine-learning/_000_hello_machine/_000_basic/_005_learn_evening/_003_iris.py
@@ -0,0 +1,5 @@
+import tensorflow as tf
+import pandas as pd
+
+
+file_path = ""
