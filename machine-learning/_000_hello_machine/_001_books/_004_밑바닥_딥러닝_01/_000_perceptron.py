ab99e23dd8570296f2de4497a78b301f393f1a98
ps <wiv821253@gmail.com>
2020-08-23 22:48:27 +0900

    add NAND

--- machine-learning/_000_hello_machine/_001_books/_004_밑바닥_딥러닝_01/_000_perceptron.py
+++ machine-learning/_000_hello_machine/_001_books/_004_밑바닥_딥러닝_01/_000_perceptron.py
@@ -1,3 +1,6 @@
+import numpy as np
+
+
 def AND(x1, x2):
     w1, w2, theta = 0.5, 0.5, 0.7
 
@@ -12,3 +15,40 @@ def AND(x1, x2):
 
 print(AND(1, 1))
 print(AND(0, 1))
+print("=================+")
+
+
+def AND2(x1, x2):
+    x = np.array([x1, x2])
+    w = np.array([0.5, 0.5])
+    b = -0.7
+    tmp = np.sum(w * x) + b
+    if tmp <= 0:
+        return 0
+    else:
+        return 1
+
+
+print(AND2(0, 1))
+print(AND2(1, 1))
+print("==================================================")
+
+
+def NAND(x1, x2):
+    x = np.array([x1, x2])
+    w = np.array([-0.5, -0.5])
+    b = 0.7
+
+    tmp = np.sum(x * w) + b
+
+    if tmp <= 0:
+        return 0
+    else:
+        return 1
+
+
+print(NAND(1, 1))  # 0
+print(NAND(0, 1))  # 1
+
+print("=-================")
+
