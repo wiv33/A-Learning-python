684f213fcaaf58521201a7fa86f0f60920d6b3a5
ps <wiv821253@gmail.com>
2020-08-24 16:40:12 +0900

    신경망 내적

--- machine-learning/_000_hello_machine/_001_books/_004_밑바닥_딥러닝_01/_005_신경망의_내적.py
+++ machine-learning/_000_hello_machine/_001_books/_004_밑바닥_딥러닝_01/_005_신경망의_내적.py
@@ -8,3 +8,19 @@ assert W.shape == (2, 3)
 
 Y = np.dot(X, W)
 assert Y.shape == (3,)
+
+# ====================================================
+
+X = np.array([1.0, .5])
+W1 = np.array([[.1, .3, .5], [.2, .4, .6]])
+B1 = np.array([.1, .2, .3])
+
+assert X.shape == (2,)
+assert W1.shape == (2, 3)
+assert B1.shape == (3,)
+
+dot = np.dot(X, W1)
+assert np.array_equal(dot, [.2, .5, .8])
+
+A1 = dot + B1
+assert np.array_equal(A1, [.30000000000000004, .7, 1.1])
