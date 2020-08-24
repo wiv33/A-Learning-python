4995066786d2800df5ce95a1ca5899f74e27f8c8
ps <wiv821253@gmail.com>
2020-08-24 22:40:45 +0900

    add softmax of inf data

--- machine-learning/_000_hello_machine/_001_books/_004_밑바닥_딥러닝_01/_007_softmax.py
+++ machine-learning/_000_hello_machine/_001_books/_004_밑바닥_딥러닝_01/_007_softmax.py
@@ -2,15 +2,24 @@ import numpy as np
 
 
 def softmax(a):
-    exp_a = np.exp(a)
-    print("1", exp_a)
+    c = np.max(a)  # inf 방지로 line 로직 추가
+    exp_a = np.exp(a - c)  # inf 방지로 (- c) 로직 추가
+    # print("1", exp_a)
     sum_exp_a = np.sum(exp_a)
-    print("2", sum_exp_a)
+    # print("2", sum_exp_a)
     y = exp_a / sum_exp_a
-    print("3", y)
+    # print("3", y)
     return y
 
 
 a = np.array([.3, 2.9, 4.])
+# a = np.array([1010, 1000, 999])
 res = softmax(a)
+print(res)
+# [0.01821127 0.24519181 0.73659691]
+# 1.8%, 24.5%, 73.7%
 assert sum(res) == 1.0
+
+# softmax는 0~1.0 사이의 실수를 출력한다.
+# 출력의 총 합은 1이다.
+# 함수의 출력은 확률로 해석할 수 있다.
