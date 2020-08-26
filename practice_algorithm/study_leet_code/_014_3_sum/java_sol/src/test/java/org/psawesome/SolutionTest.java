8f05b9a82f0008fb4465e31acb53cd013f0af542
ps <wiv821253@gmail.com>
2020-08-27 07:52:20 +0900

    add Solution.py, SolutionTest.java

---
+++ practice_algorithm/study_leet_code/_014_3_sum/java_sol/src/test/java/org/psawesome/SolutionTest.java
@@ -0,0 +1,21 @@
+package org.psawesome;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author ps [https://github.com/wiv33/learning-python]
+ * @role
+ * @responsibility
+ * @cooperate {
+ * input:
+ * output:
+ * }
+ * @see
+ * @since 20. 8. 27. Thursday
+ */
+public class SolutionTest {
+  @Test
+  void testArrFirst() {
+
+  }
+}
--- practice_algorithm/study_leet_code/_014_3_sum/three_sum_of_zero.py
+++ practice_algorithm/study_leet_code/_014_3_sum/three_sum_of_zero.py
@@ -4,14 +4,38 @@ class Solution:
     def threeSum(self, nums: [int]) -> [[int]]:
         result = []
         nums.sort()
-        left_idx = 1
+        idx = 0
+        left_idx = idx + 1
         right_idx = len(nums) - 1
 
-        for i in range(len(nums) - 2):
-            tot_sum = nums[i] + nums[left_idx] + nums[right_idx]
-            if tot_sum == 0:
-                print(nums[i], nums[left_idx], nums[right_idx])
+        while idx < (len(nums) - 2):
+            if idx > 0 and nums[idx] == nums[idx - 1]:
+                idx += 1
+                left_idx = idx + 1
+                right_idx = len(nums) - 1
+                continue
+            tot_sum = nums[idx] + nums[left_idx] + nums[right_idx]
+            # print("t {}".format(tot_sum))
+            if tot_sum < 0:
+                left_idx += 1
+            elif tot_sum > 0:
+                right_idx -= 1
+            else:
+                result.append([nums[idx], nums[left_idx], nums[right_idx]])
 
-            left_idx += 1
-            right_idx -= 1
+                while left_idx < right_idx and nums[left_idx] == nums[left_idx + 1]:
+                    left_idx += 1
+
+                while right_idx > left_idx and nums[right_idx] == nums[right_idx - 1]:
+                    right_idx -= 1
+
+                left_idx += 1
+                right_idx -= 1
+
+            if idx < len(nums) - 1 and left_idx >= right_idx:
+                idx += 1
+                left_idx = idx + 1
+                right_idx = len(nums) - 1
+
+        print(result)
         return result
