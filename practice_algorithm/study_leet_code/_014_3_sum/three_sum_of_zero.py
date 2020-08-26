a4d207da94b5033f90f59368e7fd39571f92ebfc
ps <wiv821253@gmail.com>
2020-08-26 22:48:07 +0900

    add sum of zero

--- practice_algorithm/study_leet_code/_014_3_sum/three_sum_of_zero.py
+++ practice_algorithm/study_leet_code/_014_3_sum/three_sum_of_zero.py
@@ -2,5 +2,16 @@
 
 class Solution:
     def threeSum(self, nums: [int]) -> [[int]]:
+        result = []
+        nums.sort()
+        left_idx = 1
+        right_idx = len(nums) - 1
 
-        return [[]]
+        for i in range(len(nums) - 2):
+            tot_sum = nums[i] + nums[left_idx] + nums[right_idx]
+            if tot_sum == 0:
+                print(nums[i], nums[left_idx], nums[right_idx])
+
+            left_idx += 1
+            right_idx -= 1
+        return result
