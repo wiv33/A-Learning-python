dd53175aca400085a3c7c6df589ec3bb49093ce8
ps <wiv821253@gmail.com>
2020-08-27 08:29:11 +0900

    init java Assertions

---
+++ practice_algorithm/study_leet_code/_014_3_sum/java_sol/src/test/java/org/psawesome/Solution.java
@@ -0,0 +1,21 @@
+package org.psawesome;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author ps [https://github.com/wiv33/learning-python]
+ * @role
+ * @responsibility
+ * @cooperate {
+ * input:
+ * output:
+ * }
+ * @see
+ * @since 20. 8. 27. Thursday
+ */
+public class Solution {
+  public List<List<Integer>> threeSum(int[] ints) {
+    return Arrays.asList(Arrays.asList(-1, 0, 1), Arrays.asList(-1, -1, 2));
+  }
+}
--- practice_algorithm/study_leet_code/_014_3_sum/java_sol/src/test/java/org/psawesome/SolutionTest.java
+++ practice_algorithm/study_leet_code/_014_3_sum/java_sol/src/test/java/org/psawesome/SolutionTest.java
@@ -1,6 +1,12 @@
 package org.psawesome;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * @author ps [https://github.com/wiv33/learning-python]
@@ -13,9 +19,24 @@ import org.junit.jupiter.api.Test;
  * @see
  * @since 20. 8. 27. Thursday
  */
-public class SolutionTest {
-  @Test
-  void testArrFirst() {
+class SolutionTest {
+  Solution solution;
 
+  @BeforeEach
+  void setUp() {
+    solution = new Solution();
+  }
+
+  @AfterEach
+  void tearDown() {
+    solution = null;
+  }
+
+  @Test
+  void testFirstLoop() {
+    final int[] ints = {-1, 0, 1, 2, -1, -4};
+    List<List<Integer>> actual = solution.threeSum(ints);
+    List<List<Integer>> expected = Arrays.asList(Arrays.asList(-1, 0, 1), Arrays.asList(-1, -1, 2));
+    Assertions.assertArrayEquals(new List[]{expected}, new List[]{actual});
   }
 }
