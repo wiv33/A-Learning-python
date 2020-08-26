6de842a8283911179ceb87e00f11d8d9207a6755
ps <wiv821253@gmail.com>
2020-08-26 11:07:53 +0900

    reset

--- practice_algorithm/study_leet_code/_008_palindrome_number/palindrome_number.py
+++ practice_algorithm/study_leet_code/_008_palindrome_number/palindrome_number.py
@@ -3,19 +3,4 @@
 
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-        if x <= 0:
-            return False
-
-        max_val = 10
-        while x // max_val != 0:
-            max_val *= 10
-
-        loop = 1
-        # max_val //= 10
-        while max_val >= loop:
-            f, s = x // max_val, x // loop
-            print(f, s)
-            # print(max_val, loop, end='\n')
-            max_val //= 10
-            loop *= 10
         return True
