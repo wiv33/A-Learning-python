7c5d8f8f04c126ed704c4e198ba47702b2cac17c
ps <wiv821253@gmail.com>
2020-08-27 07:52:20 +0900

    add Solution.py, SolutionTest.java

---
+++ practice_algorithm/study_leet_code/_014_3_sum/java_sol/build.gradle.kts
@@ -0,0 +1,20 @@
+plugins {
+    java
+}
+java.sourceCompatibility = JavaVersion.VERSION_13
+
+allprojects {
+    apply(plugin = "java")
+
+    repositories {
+        mavenCentral()
+    }
+    dependencies {
+        extra["jupiterVersion"] = "5.6.2"
+        testImplementation("org.junit.jupiter:junit-jupiter-api:${extra["jupiterVersion"]}")
+        testImplementation("org.junit.jupiter:junit-jupiter-engine:${extra["jupiterVersion"]}")
+    }
+    tasks.withType<Test> {
+        useJUnitPlatform()
+    }
+}
--- practice_algorithm/study_leet_code/_014_3_sum/test_three_sum_of_zero.py
+++ practice_algorithm/study_leet_code/_014_3_sum/test_three_sum_of_zero.py
@@ -10,3 +10,9 @@ class TestSolution(TestCase):
             [-1, 0, 1],
             [-1, -1, 2]
         ], actual)
+
+    def test_three_sum_(self):
+        actual = Solution().threeSum([-4, -2, -2, -2, 0, 1, 2, 2, 2, 3, 3, 4, 4, 6, 6])
+        self.assertListEqual(
+            [[-4, -2, 6], [-4, 0, 4], [-4, 1, 3], [-4, 2, 2], [-2, -2, 4], [-2, 0, 2]]
+            , actual)
