965a5d2f16d8d2524ec19b913acbb8d138bddcc6
ps <wiv821253@gmail.com>
2020-08-29 23:59:50 +0900

    is palindrome part 1

---
+++ practice_algorithm/파이썬_인터뷰/_010_linked_list/_000_palindrome_linked_list.py
@@ -0,0 +1,29 @@
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+def is_palindrome_the_linked_list(head: ListNode) -> bool:
+    q: [] = []
+    if not head:
+        return True
+
+    node = head
+
+    while node:
+        q.append(node.val)
+        node = node.next
+
+    while len(q) > 1:
+        if q.pop(0) != q.pop():
+            return False
+
+    return True
+
+
+result = is_palindrome_the_linked_list(ListNode(1, ListNode(2, ListNode(2, ListNode(1)))))
+assert result
+
+res = is_palindrome_the_linked_list(ListNode(1, ListNode(2)))
+assert not res
