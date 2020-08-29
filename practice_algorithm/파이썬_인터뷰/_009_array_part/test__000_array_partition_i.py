9845c06be109086ab936ce894208c7698f54f1b3
ps <wiv821253@gmail.com>
2020-08-29 23:26:59 +0900

    add array pair sum

--- practice_algorithm/파이썬_인터뷰/_009_array_part/test__000_array_partition_i.py
+++ practice_algorithm/파이썬_인터뷰/_009_array_part/test__000_array_partition_i.py
@@ -7,3 +7,7 @@ class TestSolution(TestCase):
     def test_array_pair_sum(self):
         actual = Solution([1, 4, 2, 3]).array_pair_sum()
         self.assertEqual(4, actual)
+
+    def test_array_pair_sum_python(self):
+        actual = Solution([1, 4, 2, 3]).array_pair_sum_python()
+        self.assertEqual(4, actual)
