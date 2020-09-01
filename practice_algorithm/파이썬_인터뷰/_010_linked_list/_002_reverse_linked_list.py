41918e77a7fddc65d338edf2cdef56c2cd02bb4d
ps <wiv821253@gmail.com>
2020-09-01 20:28:57 +0900

    init reverse linked list

---
+++ practice_algorithm/파이썬_인터뷰/_010_linked_list/_002_reverse_linked_list.py
@@ -0,0 +1,8 @@
+class ListNode:
+    def __init__(self, val: int, next_node):
+        self.val: int = val
+        self.next: ListNode = next_node
+
+
+def reverse_linked_list(head: ListNode) -> ListNode:
+    pass
