141ae2c736e8a8b7cf3cf8d1acb62b36b82cc889
ps <wiv821253@gmail.com>
2020-09-01 23:00:13 +0900

    add other

--- practice_algorithm/파이썬_인터뷰/_010_linked_list/_002_reverse_linked_list.py
+++ practice_algorithm/파이썬_인터뷰/_010_linked_list/_002_reverse_linked_list.py
@@ -18,3 +18,22 @@ res = reverse_linked_list(ListNode(1, ListNode(2, ListNode(3, ListNode(4, ListNo
 while res:
     print(res.val)
     res = res.next
+
+print("=== end ===")
+
+
+def reverse_linked_list_other(head: ListNode) -> ListNode:
+    def reverse_node(node: ListNode, prev: ListNode = None) -> ListNode:
+        if not node:
+            return prev
+
+        next_node, node.next = node.next, prev
+        return reverse_node(next_node, node)
+
+    return reverse_node(head)
+
+
+res = reverse_linked_list_other(ListNode(1, ListNode(2, ListNode(3, ListNode(4, ListNode(5))))))
+while res:
+    print(res.val)
+    res = res.next
