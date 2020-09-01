bd2bcc1f4221121090559fd44ddcda5f204c135a
ps <wiv821253@gmail.com>
2020-09-01 22:49:53 +0900

    add reverseList

--- practice_algorithm/파이썬_인터뷰/_010_linked_list/_002_reverse_linked_list.py
+++ practice_algorithm/파이썬_인터뷰/_010_linked_list/_002_reverse_linked_list.py
@@ -1,8 +1,19 @@
 class ListNode:
-    def __init__(self, val: int, next_node):
+    def __init__(self, val: int = 0, next_node=None):
         self.val: int = val
         self.next: ListNode = next_node
 
 
-def reverse_linked_list(head: ListNode) -> ListNode:
-    pass
+def reverse_linked_list(head: ListNode, prev: ListNode = None) -> ListNode:
+    if not head:
+        return prev
+
+    next_node, head.next = head.next, prev
+    return reverse_linked_list(next_node, head)
+
+
+res = reverse_linked_list(ListNode(1, ListNode(2, ListNode(3, ListNode(4, ListNode(5))))))
+
+while res:
+    print(res.val)
+    res = res.next
