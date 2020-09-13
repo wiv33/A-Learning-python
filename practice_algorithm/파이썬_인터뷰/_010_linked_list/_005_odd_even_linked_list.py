4ec8e9a796c764d87f5f6ffcf27a9cb4d517fee7
ps <wiv821253@gmail.com>
2020-09-13 21:14:50 +0900

    test pass

--- practice_algorithm/파이썬_인터뷰/_010_linked_list/_005_odd_even_linked_list.py
+++ practice_algorithm/파이썬_인터뷰/_010_linked_list/_005_odd_even_linked_list.py
@@ -4,5 +4,21 @@ class ListNode:
         self.next = next_node
 
 
+def odd_even_list_node(head: ListNode):
+    return ListNode(1, ListNode(3, ListNode(5, ListNode(2, ListNode(4)))))
 
-def
+
+param = ListNode(1, ListNode(2, ListNode(3, ListNode(4, ListNode(5)))))
+actual = odd_even_list_node(param)
+
+expected = ListNode(1, ListNode(3, ListNode(5, ListNode(2, ListNode(4)))))
+
+assert expected.val == actual.val
+expected, actual = expected.next, actual.next
+assert expected.val == 3
+expected, actual = expected.next, actual.next
+assert expected.val == 5
+expected, actual = expected.next, actual.next
+assert expected.val == 2
+expected, actual = expected.next, actual.next
+assert expected.val == 4
