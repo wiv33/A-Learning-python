44c035b1fa238778b556a923b6ef528f311f5d37
ps <wiv821253@gmail.com>
2020-09-20 23:33:10 +0900

    손글씨 데이터의 코드 리뷰를 통한 모델의 학습 되짚기

--- machine-learning/_000_hello_machine/_001_books/_005_케라스_창시자_딥러닝/_003_gradient_optimizer.py
+++ machine-learning/_000_hello_machine/_001_books/_005_케라스_창시자_딥러닝/_003_gradient_optimizer.py
@@ -15,4 +15,36 @@ f(x + epsilon_x) = y + epsilon_y
 기울기의 방향과 기울기 값을 알아보는 계산 엑셀 주소 (코딩 야학)
 https://docs.google.com/spreadsheets/d/1XfE4HvjhqxU335DuW5-vHdX6ssVMfiaBCb1p9jN-U6Y/edit#gid=0
 
+
+#
+텐서 연산의 번화율 - gradient
+# W를 input x에 행렬곱 했을 때 예측값 y의 영향도
+y_pred = dot(W, x)
+
+# y 예측과 y의 차이가 오류값이 된다.
+loss_value = loss(y_pred, y)
+
+"""
+
+# nesterov momentum
+"""
+
+past_velocity = 0.
+momentum = 0.1
+while loss > 0.01:
+    w, loss, gradient = get_current_parameters()
+    velocity = momentum * past_velocity - learning_date * gradient
+    w = w + momentum * velocity - learning_rate * gradient
+    past_velocity = velocity
+    update_parameter(w)
+"""
+
+from keras import optimizers
+# 일반적으로 모멘텀은 0.9를 많이 사용한다.
+sgd = optimizers.SGD(lr=0.01, momentum=0.9, nesterov=True)
+network.compile(optimizers=sgd)
+
+"""
+변화율 연결: 역전파 알고리즘
+backpropagation
 """
