555eaa1640ace446ee125d996a323ec4d5e62d08
PSK <wiv821253@gmail.com>
2020-10-03 12:58:26 +0900

    add turtle

---
+++ sundry/turtle/_000_거북이_랜덤.py
@@ -0,0 +1,12 @@
+import turtle
+import random
+
+t = turtle.Turtle()
+t.shape('turtle')
+
+for i in range(30):
+    length = random.randint(1, 100)
+    t.forward(length)
+    angle = random.randint(-180, 180)
+    t.right(angle)
+
