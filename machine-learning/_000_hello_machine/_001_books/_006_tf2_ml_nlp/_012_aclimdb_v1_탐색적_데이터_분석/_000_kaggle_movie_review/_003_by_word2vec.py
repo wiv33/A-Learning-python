9ea5e4bf8d4d6bcfe85edf4f82c43ec5f1fdbb4d
ps <wiv821253@gmail.com>
2020-10-10 17:48:10 +0900

    정렬 변경

--- machine-learning/_000_hello_machine/_001_books/_006_tf2_ml_nlp/_012_aclimdb_v1_탐색적_데이터_분석/_000_kaggle_movie_review/_003_by_word2vec.py
+++ machine-learning/_000_hello_machine/_001_books/_006_tf2_ml_nlp/_012_aclimdb_v1_탐색적_데이터_분석/_000_kaggle_movie_review/_003_by_word2vec.py
@@ -3,15 +3,17 @@ import pandas as pd
 DATA_IN_PATH = './data_in/refine/'
 TRAIN_CLEAN_DATA = 'train_clean.csv'
 
+
 def rf(path):
     return "{}{}".format(DATA_IN_PATH, path)
+
 
 train_data = pd.read_csv(rf(TRAIN_CLEAN_DATA))
 
 reviews = list(train_data['review'])
 sentiments = list(train_data['sentiment'])
 
-sentences =  []
+sentences = []
 
 for rev in reviews:
     sentiments.append(rev.split())
