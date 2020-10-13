318e6d01097e236038ab09eac29abd4946be7fb2
ps <wiv821253@gmail.com>
2020-10-13 23:31:10 +0900

    add func

--- machine-learning/_000_hello_machine/_002_examples/_001_first_v2.py
+++ machine-learning/_000_hello_machine/_002_examples/_001_first_v2.py
@@ -12,3 +12,19 @@ train_df = pd.read_csv(cp('train.csv'))
 user_tag_df = pd.read_csv(cp('user_tags.csv'))
 job_companies_df = pd.read_csv(cp('job_companies.csv'))
 job_tag_df = pd.read_csv(cp('job_tags.csv'))
+
+
+def grouping_tags(df: pd.DataFrame, id: str) -> {}:
+    result = []
+    for u, t in df.groupby(id):
+        temp = {}
+        temp[id] = u
+        temp['{}_tags'.format(id)] = " ".join(df[df[id] == u]['tagID'].values)
+        result.append(temp)
+
+    return pd.DataFrame(result)
+
+
+job_match_df = grouping_tags(job_tag_df, 'jobID')
+user_match_df = grouping_tags(user_tag_df, 'userID')
+
