ba2055b8ec0b11dbcca16c66e4355b5cf6075397
PSK <wiv821253@gmail.com>
2020-10-15 12:32:45 +0900

    optimizer

--- machine-learning/_000_hello_machine/_000_basic/_006_multi_camp_tf_2_0/_002_subclassing_example.py
+++ machine-learning/_000_hello_machine/_000_basic/_006_multi_camp_tf_2_0/_002_subclassing_example.py
@@ -7,21 +7,22 @@ Original file is located at
     https://colab.research.google.com/drive/1VXVuYDb8oyWMxabO94JX1-sU8JmyJ_Aw
 """
 
-from tensorflow import keras
 import tensorflow as tf
 
 
-class CustomModel(keras.Model):
+class CustomModel(tf.keras.Model):
     def __init__(self):
         super(CustomModel, self).__init__(name='subclassing_exercise')
-        self.flatten = keras.layers.Flatten()
-        self.fully_connected_1 = keras.layers.Dense(256, activation='relu')
-        self.fc2 = keras.layers.Dense(128, activation='relu')
-        self.dropout = keras.layers.Dropout(rate=0.2)
-        self.fc3 = keras.layers.Dense(10, activation='softmax')
+        self.flatten = tf.keras.layers.Flatten()
+        self.fully_connected_1 = tf.keras.layers.Dense(256, activation='relu')
+        self.fc2 = tf.keras.layers.Dense(128, activation='relu')
+        self.dropout = tf.keras.layers.Dropout(rate=0.2)
+        self.fc3 = tf.keras.layers.Dense(10, activation='softmax')
 
     def call(self, x):
-        """ 생성자에 정의한 모델을 실행시키는 구간으로 input에 해당하는 x를 받아야 한다. """
+        """ 생성자에 정의한 모델을 실행시키는 구간으로 input에 해당하는 x를 받아야 한다.
+        :param x
+        """
         x = self.flatten(x)
         x = self.fully_connected_1(x)
         x = self.fc2(x)
@@ -35,7 +36,7 @@ model = CustomModel()
 model.compile(optimizer='adam',
               loss='sparse_categorical_crossentropy',
               metrics=['accuracy'])
-data = keras.datasets.fashion_mnist.load_data()
+data = tf.keras.datasets.fashion_mnist.load_data()
 
 (x_train, y_train), (x_test, y_test) = data
 
