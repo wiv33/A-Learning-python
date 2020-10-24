fea6d09ed32883a51b9baba62fc65df271b8dad3
PSK <40161102+wiv33@users.noreply.github.com>
2020-10-24 11:39:57 +0900

    weather data push

--- machine-learning/_000_hello_machine/_000_basic/_003_tensorflow_cuk_edu/_002_2_ml/_002_weather_analysis.py
+++ machine-learning/_000_hello_machine/_000_basic/_003_tensorflow_cuk_edu/_002_2_ml/_002_weather_analysis.py
@@ -1,4 +1,6 @@
 import pandas as pd
 
-df = pd.read_csv('./data/direction_of_wind_201910_202009.csv', encoding='utf-8')
+df = pd.read_csv('./data/월곡제1동_강수_201910_202009.csv', encoding='utf-8')
+columns = ['day', 'hour', 'precipitation']
+df.columns = columns
 print(df.head())
