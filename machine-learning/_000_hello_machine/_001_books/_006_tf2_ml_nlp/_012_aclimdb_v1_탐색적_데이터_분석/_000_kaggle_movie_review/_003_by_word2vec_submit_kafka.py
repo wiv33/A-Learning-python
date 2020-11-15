265472fecb36fe3000e0a5580ed5d85da7d1e564
PSK <40161102+wiv33@users.noreply.github.com>
2020-11-15 12:47:12 +0900

    add KafkaManager.py

---
+++ machine-learning/_000_hello_machine/_001_books/_006_tf2_ml_nlp/_012_aclimdb_v1_탐색적_데이터_분석/_000_kaggle_movie_review/_003_by_word2vec_submit_kafka.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import json
+from KafkaManager import KafkaManager
+
+
+manager = KafkaManager()
+consumer = manager.consumer_message(topic='word2vec-nlp-tutorial', group_id='nlp-consumer')
+
+df = None
+df_list = []
+for msg in consumer:
+    df_list.append(pd.DataFrame(json.load(msg.value.decode('UTF-8'))))
+
+print(len(df_list))
