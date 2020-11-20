c8f7fca71027e2ebd0a8c89c2fdb9faf132c5202
PSK <wiv821253@gmail.com>
2020-11-20 17:52:33 +0900

    kafka producer aws ip 수정

--- my-kafka/_000_basic/_001_on_aws/_000_producer.py
+++ my-kafka/_000_basic/_001_on_aws/_000_producer.py
@@ -1,6 +1,9 @@
 from kafka import KafkaProducer
 
-bootstrap_servers = ','.join(['52.79.184.210:9092', '52.79.184.210:9093', '52.79.184.210:9094'])
+public_ip = "52.79.184.210"
+
+bootstrap_servers = "{0}:9092,{0}:9093,{0}:9094".format(public_ip)
+# bootstrap_servers = ','.join(['52.79.184.210:9092', '52.79.184.210:9093', '52.79.184.210:9094'])
 producer = KafkaProducer(acks=1,
                          compression_type='gzip',
                          bootstrap_servers=bootstrap_servers)
