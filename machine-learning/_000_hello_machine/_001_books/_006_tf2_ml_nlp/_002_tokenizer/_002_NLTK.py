1f2b1ed55cf8933df3674185684698fc9bb872e6
PSK <40161102+wiv33@users.noreply.github.com>
2020-11-30 20:59:23 +0900

    add subclass, nltk test code

--- machine-learning/_000_hello_machine/_001_books/_006_tf2_ml_nlp/_002_tokenizer/_002_NLTK.py
+++ machine-learning/_000_hello_machine/_001_books/_006_tf2_ml_nlp/_002_tokenizer/_002_NLTK.py
@@ -11,9 +11,16 @@ sentence = "Natural language processing (NLP) is a subfield of computer science,
            "and artificial intelligence concerned with the interactions between computers and human (natural) " \
            "languages, in particular how to program computers to process and analyze large amounts of natural " \
            "language data."
-print(word_tokenize(sentence))
+# print(word_tokenize(sentence))
 # ['Natural', 'language', 'processing', '(', 'NLP', ')', 'is', 'a', 'subfield', 'of', 'computer', 'science', ',',
 # 'information', 'engineering', ',', 'and', 'artificial', 'intelligence', 'concerned', 'with', 'the', 'interactions',
 # 'between', 'computers', 'and', 'human', '(', 'natural', ')', 'languages', ',', 'in', 'particular', 'how', 'to',
 # 'program', 'computers', 'to', 'process', 'and', 'analyze', 'large', 'amounts', 'of', 'natural', 'language', 'data',
 # '.']
+
+
+from tensorflow.keras.datasets import reuters
+
+(X_train, y_train), (X_test, y_test) = reuters.load_data(num_words=None, test_split=0.2)
+
+
