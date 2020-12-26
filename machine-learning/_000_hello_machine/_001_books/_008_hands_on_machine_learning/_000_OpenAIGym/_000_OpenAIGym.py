c962974ee81990469154fd0e26ffac572e0c61e2
PSK <40161102+wiv33@users.noreply.github.com>
2020-12-27 01:37:15 +0900

    observation, reward, done, info 확인하기 // openAI

--- machine-learning/_000_hello_machine/_001_books/_008_hands_on_machine_learning/_000_OpenAIGym/_000_OpenAIGym.py
+++ machine-learning/_000_hello_machine/_001_books/_008_hands_on_machine_learning/_000_OpenAIGym/_000_OpenAIGym.py
@@ -1,4 +1,5 @@
 import gym
+import numpy as np
 
 env = gym.make('CartPole-v1')
 obs = env.reset()
@@ -8,8 +9,32 @@ print(obs)
 img = env.render(mode='rgb_array')
 print(img.shape)
 
-action = 10
+action = 1
 obs, reward, done, info, = env.step(action)
 print(obs)
 
-env.close()
+
+def basic_policy(obs):
+    angle = obs[2]
+    return 0 if angle < 0 else 1
+
+
+totals = []
+
+for episode in range(300):
+    episode_rewards = []
+    obs = env.reset()
+    for step in range(200):
+        action = basic_policy(obs)
+        obs, reward, done, info = env.step(action)
+        episode_rewards.append(reward)
+        if done:
+            break
+
+    totals.append(np.sum(episode_rewards))
+
+print(f'** result info **\n'
+      f'mean: {np.mean(totals)}\n'
+      f'std: {np.std(totals)}\n'
+      f'min: {np.mean(totals)}\n'
+      f'max: {np.max(totals)}')
