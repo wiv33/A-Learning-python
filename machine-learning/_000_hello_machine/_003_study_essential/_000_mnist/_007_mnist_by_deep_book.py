2829fa908edfaeb43c8b367cb6719154969a90b7
PSK <40161102+wiv33@users.noreply.github.com>
2021-01-19 22:40:24 +0900

    mnist review

--- machine-learning/_000_hello_machine/_003_study_essential/_000_mnist/_007_mnist_by_deep_book.py
+++ machine-learning/_000_hello_machine/_003_study_essential/_000_mnist/_007_mnist_by_deep_book.py
@@ -1,4 +1,5 @@
-﻿import tensorflow as tf
+﻿import matplotlib.pyplot as plt
+import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import utils, layers, models
 
@@ -7,7 +8,7 @@ print(tf.__version__)
 # 신경망과 훈련 매개변수
 EPOCHS = 200
 BATCH_SIZE = 128
-VERBEOSE = 1
+VERBOSE = 1
 NB_CLASSES = 10  # output의 demension
 N_HIDDEN = 128
 VALIDATION_SPLIT = 0.2  # 검증
@@ -26,33 +27,48 @@ x_test = x_test.reshape(10000, RESHAPED)
 x_train = x_train.astype('float32')
 x_test = x_test.astype('float32')
 
-# scaling
-x_train, x_test = x_train / 255., x_test / 255.
+# 입력을 [0, 1] 사이로 정규화
+# 파이썬의 숫자 타입
+# int, float
+x_train = x_train / 255.
+x_test = x_test / 255.
 
+print(x_train.shape[0], 'train samples')
+print(x_test.shape[0], 'test samples')
+
+# 레이블을 원핫 인코딩
 y_train = utils.to_categorical(y_train, NB_CLASSES)
 y_test = utils.to_categorical(y_test, NB_CLASSES)
 
-inputs = layers.Input(shape=RESHAPED)
-H = layers.Dense(10, name='dense_layer_1')(inputs)
-outputs = layers.Activation(activation='softmax')(H)
+# 모델 구축
+model = models.Sequential()
 
-model = models.Model(inputs, outputs)
+model.add(layers.Dense(NB_CLASSES,
+                       input_shape=(RESHAPED,),
+                       name='dense_layer',
+                       activation='softmax'))
 
-model.compile(optimizer='adam',
-              loss='categorical_crossentropy',
-              metrics=['accuracy', 'mse'])
-
-history = model.fit(x_train, y_train,
-                    epochs=10,
-                    batch_size=128)
-
-import matplotlib.pyplot as plt
-
-plt.plot(history.history['accuracy'])
-plt.plot(history.history['loss'])
-
+plt.imshow(x_train[0].reshape(28, 28))
 plt.show()
 
-evaluate = model.evaluate(x_test, y_test)
+# 모델 컴파일
+model.compile(optimizer="SGD",
+              loss='categorical_crossentropy',
+              metrics=['accuracy'])
 
-print('eval ', evaluate)
+model.fit(x_train,
+          y_train,
+          batch_size=BATCH_SIZE,
+          epochs=EPOCHS,
+          verbose=VERBOSE,
+          validation_split=VALIDATION_SPLIT)
+
+# 모델 평가
+# a, b = ("a", "b")
+test_loss, test_acc = model.evaluate(x_test, y_test)
+
+test_loss
+
+test_acc
+
+model.summary()
