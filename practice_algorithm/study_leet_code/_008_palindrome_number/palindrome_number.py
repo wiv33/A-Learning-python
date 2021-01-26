9793c3524784c9fd43944a38c5e43b0df95d0d14
PSK <40161102+wiv33@users.noreply.github.com>
2021-01-27 01:23:37 +0900

    palindrome

--- practice_algorithm/study_leet_code/_008_palindrome_number/palindrome_number.py
+++ practice_algorithm/study_leet_code/_008_palindrome_number/palindrome_number.py
@@ -3,4 +3,15 @@
 
 class Solution:
     def isPalindrome(self, x: int) -> bool:
+        if x < 0:
+            return False
+        data = str(x)
+        start, last = 0, len(data) - 1
+
+        while start <= last:
+            if data[start] != data[last]:
+                return False
+            start += 1
+            last -= 1
+
         return True
