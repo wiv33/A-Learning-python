2622c7f51cb1d3729f8a41b0866f42cb2ab68722
PSK <40161102+wiv33@users.noreply.github.com>
2021-01-28 00:41:19 +0900

    update assert

---
+++ practice_algorithm/study_leet_code/_009_regular_expression_matching/test_regular_experssion.py
@@ -0,0 +1,20 @@
+import unittest
+from RegularExpression import is_match
+
+
+class MyTestCase(unittest.TestCase):
+    def test_aa_a(self):
+        self.assertFalse(is_match('aa', 'a'))
+
+    def test_aa_aAll(self):
+        self.assertTrue(is_match('aa', 'a*'))
+
+    def test_ab_dotAll(self):
+        self.assertTrue(is_match('ab', '.*'))
+
+    def test_mississippi_false(self):
+        self.assertFalse(is_match("mississippi", "mis*is*p*."))
+
+
+if __name__ == '__main__':
+    unittest.main()
