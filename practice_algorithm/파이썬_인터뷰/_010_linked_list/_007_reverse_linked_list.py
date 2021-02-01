ad06affbead1bf41573eb13583e54d66ab0bc901
PSK <40161102+wiv33@users.noreply.github.com>
2021-02-02 01:24:10 +0900

    reverse list

---
+++ practice_algorithm/파이썬_인터뷰/_010_linked_list/_007_reverse_linked_list.py
@@ -0,0 +1,42 @@
+from _006_merge_two_sorted_lists import ListNode
+
+
+# https://leetcode.com/problems/reverse-lined-list/
+#
+
+# input: 1->2->3->4->5->null
+# output: 5->4->3->2->1->null
+
+def reverse_list(head: ListNode) -> ListNode:
+    def reverse(node: ListNode, prev: ListNode = None):
+        if not node:
+            return prev
+
+        n, node.next = node.next, prev
+        return reverse(n, node)
+
+    return reverse(head)
+
+
+def reverse_list_assert(func):
+    actual = func(ListNode(1, ListNode(2, ListNode(3, ListNode(4, ListNode(5))))))
+    expected = ListNode(5, ListNode(4, ListNode(3, ListNode(2, ListNode(1)))))
+
+    while actual:
+        assert expected.val == actual.val
+        actual, expected = actual.next, expected.next
+
+
+def reverse_list_for(head: ListNode) -> ListNode:
+    node, prev = head, None
+
+    while node:
+        n, node.next = node.next, prev
+        prev, node = node, n
+
+    return prev
+
+
+if __name__ == '__main__':
+    reverse_list_assert(reverse_list)
+    reverse_list_assert(reverse_list_for)
