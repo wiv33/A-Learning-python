caf97dd5ac089d27dfc85146f3b78b0b716d62a5
PSK <40161102+wiv33@users.noreply.github.com>
2021-02-01 23:40:31 +0900

    update assert

--- practice_algorithm/파이썬_인터뷰/_010_linked_list/_006_metge-two-sorted-lists.py
+++ practice_algorithm/파이썬_인터뷰/_010_linked_list/_006_metge-two-sorted-lists.py
@@ -10,11 +10,37 @@ from _005_odd_even_linked_list import ListNode
 
 
 def merge_two_lists_recursive(l1, l2) -> ListNode:
+    # 우선순위
+    # 1. l1.val > l2.val
+    # 2. l2 and
+    # 3. not l1
+    # 4. or
     if (not l1) or (l2 and l1.val > l2.val):
+        # l1이 l2보다 크면 스왑
         l1, l2 = l2, l1
 
     if l1:
+        # li.next를 통해 재귀
         l1.next = merge_two_lists_recursive(l1.next, l2)
+
     return l1
 
 
+if __name__ == '__main__':
+    param_l1 = ListNode(1, ListNode(1, ListNode(2, ListNode(3, ListNode(7)))))
+    param_l2 = ListNode(2, ListNode(3, ListNode(7, ListNode(7, ListNode(9)))))
+    result = merge_two_lists_recursive(param_l1, param_l2)
+    expected = ListNode(1,
+                ListNode(1,
+                 ListNode(2,
+                  ListNode(2,
+                   ListNode(3,
+                    ListNode(3,
+                     ListNode(7,
+                      ListNode(7,
+                       ListNode(7,
+                        ListNode(9))))))))))
+
+    while result or expected:
+        assert result.val == expected.val
+        result, expected = result.next, expected.next
