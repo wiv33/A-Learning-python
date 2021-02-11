2f7b28c73ae665eb2356e5f638413e957a4043ad
PSK <40161102+wiv33@users.noreply.github.com>
2021-02-11 15:21:16 +0900

    daily temperature

---
+++ practice_algorithm/파이썬_인터뷰/_011_stack/_003_daily_temperatures.py
@@ -0,0 +1,9 @@
+# https://leetcode.com/problems/daily-temperatures
+
+
+# input: [73, 74, 75, 71, 69, 72, 76, 73]
+# output: [1, 1, 4, 2, 1, 1, 0, 0]
+
+# 매일의 화씨 온도 리스트를 입력받아서,
+# 더 따뜻한 날씨를 위해서는 며칠을 더 기다려야 하는지 출력
+
