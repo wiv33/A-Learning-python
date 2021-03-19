b11696cbecf6ff0d42be8324d309090c819f9cf6
PSK <40161102+wiv33@users.noreply.github.com>
2021-03-20 00:17:09 +0900

    add tokenizing

--- machine-learning/_000_hello_machine/_000_basic/_003_cuk_edu/_004_3_text_processing/_000_tokenizer.py
+++ machine-learning/_000_hello_machine/_000_basic/_003_cuk_edu/_004_3_text_processing/_000_tokenizer.py
@@ -4,20 +4,29 @@ from nltk.corpus import stopwords
 text = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam adipisci error suscipit minus quo assumenda ' \
        'culpa, veniam molestias dolorem voluptatum asperiores amet magni vitae est eaque reprehenderit quis vel fugit! '
 
-result = sent_tokenize(text)
+sent_text = sent_tokenize(text)
 
-print(result)
+print(sent_text)
 
 vocabulary = {}
 sentences = []
 
 stop_words = stopwords.words('english')
 
-for sent in sentences:
-       sentence = word_tokenize(sent)
-       res = []
+for sent in sent_text:
+    sentence = word_tokenize(sent)
+    res = []
 
-       for word in sentence:
-              pass
+    for word in sentence:
+        _word = word.lower()
+        if word not in stop_words and len(word) > 2:
+            res.append(word)
+            if word not in vocabulary:
+                vocabulary[word] = 0
 
+            vocabulary[word] += 1
+
+    sentences.append(res)
+
+print(sentences)
 
