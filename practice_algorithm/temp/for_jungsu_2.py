6108a9250277dbc5e50bbb02c0b9d2ea569bb85c
PSK <40161102+wiv33@users.noreply.github.com>
2021-03-20 23:51:08 +0900

    add version 2

---
+++ practice_algorithm/temp/for_jungsu_2.py
@@ -0,0 +1,19 @@
+from datetime import date, timedelta
+import datetime
+
+
+# [브론즈 기간, 실버 기간, 골드 기간, 플래티넘 기간, 다이아몬드 기간] 순서로 채워서 반환
+
+# required fuc
+# 1. full date를 넣으면 day로 환산한 일자를 반환
+# 2. grade를 계산
+
+
+def solution(purchase):
+    pass
+
+
+if __name__ == '__main__':
+    print(solution(
+        ["2019/01/30 5000", "2019/04/05 10000", "2019/06/10 20000", "2019/08/15 50000", "2019/12/01 100000"]) == [
+              245, 30, 30, 30, 30])
