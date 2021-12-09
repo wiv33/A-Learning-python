4861add8c663eca98720f02cd00d4b61d958bf23
PSK <wiv821253@gmail.com>
2021-12-09 09:33:13 +0900

    face book login

--- Crawling/_000_basic/_002_selenium/_005_facebook_login.py
+++ Crawling/_000_basic/_002_selenium/_005_facebook_login.py
@@ -9,10 +9,26 @@ import _003_daum_news_by_service
 
 
 def runner_05(d: WebDriver):
-    user_name =
-    pass
+    username = 'my account id'
+    password = 'my password'
+    username_id = '//*[@id="email"]'
+    password_id = '//*[@id="pass"]'
+    login_btn = '//*[@name="login"]'
+    username_tag = WebDriverWait(d, 5).until(ec.presence_of_element_located((By.XPATH, username_id)))
+    password_tag = WebDriverWait(d, 5).until(ec.presence_of_element_located((By.XPATH, password_id)))
+    login_btn_tag = WebDriverWait(d, 5).until(ec.presence_of_element_located((By.XPATH, login_btn)))
+
+    username_tag.clear()
+    username_tag.send_keys(username)
+    time.sleep(1)
+    password_tag.clear()
+    password_tag.send_keys(password)
+    time.sleep(1)
+
+    assert login_btn_tag is not None
+    login_btn_tag.click()
 
 
 if __name__ == '__main__':
     deco = _003_daum_news_by_service.BrowserDecorator()
-    deco.run_browser('https://www.facebook.com/', runner_05, True)
+    deco.run_browser('https://www.facebook.com/', runner_05)
