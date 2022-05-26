7c7c22f505c7395bdf0334976c39d8f173606685
PSK <wiv821253@gmail.com>
2022-05-26 22:44:37 +0900

    update money comma

--- sundry/ETC/OrderMoney.py
+++ sundry/ETC/OrderMoney.py
@@ -5,45 +5,49 @@ import collections
 def result(arr, total_price):
     print(arr)
     total_money_unit = to_unit(total_price)
-    print(f"total money : {total_money_unit}")
+    print(f"total money : {total_money_unit.__format__(',')}")
 
     report = collections.defaultdict()
     total_money = 0
     for x in arr:
         series = x.split(" ")
         length = len(series) - 1
-        name, money = clean_name(series[1]), to_digital(clean_name(series[1]), series[length].replace('\n', ''))
+        money_string = series[length].replace('\n', '')
+        if not re.sub('[\\D]', '', money_string).isdigit():
+            continue
 
+        name, money = clean_name(series), to_digital(money_string)
         if total_money >= total_money_unit:
             break
 
         if total_money_unit < (total_money + money):
             final_money = total_money_unit - total_money
             total_money += final_money
-            report.update({name: str(int(final_money / 10000)) + "만"})
-            print(name, str(int(final_money / 10000)) + "만", total_money)
+            report.update({name: str(int(money / 10000)) + "만"})
+            print(name, f"{(int(money / 10000)).__format__(',')}만", final_money.__format__(','))
             break
 
         total_money += money
         report.update({name: str(int(money / 10000)) + "만"})
-        print(name, str(int(money / 10000)) + "만", total_money)
+        print(name, f"{(int(money / 10000)).__format__(',')}만", total_money.__format__(','))
 
     return report
 
 
-def clean_name(s: str):
+def clean_name(series: []):
+    s = ''.join(series[1: series.__len__()])
     if re.search('[/]', s):
         return s[:s.find("/")]
     return s
 
 
-def to_digital(name, s: str) -> int:
+def to_digital(s: str) -> int:
     number = to_unit(s)
     return number
 
 
 def to_unit(str_num: str) -> int:
-    TO_HUNDRED_MILLION = 10_000_000
+    TO_HUNDRED_MILLION = 100_000_000
     TO_THOUSAND = 10_000_000
     TO_TEN_THOUSAND = 10_000
 
@@ -53,7 +57,8 @@ def to_unit(str_num: str) -> int:
     if re.search("억", str_num):
         first, second = re.split("[.,]", str_num)
         if len(second) - 1 > 1:
-            return int(remove_str(str_num)) * int(TO_HUNDRED_MILLION / 10)
+            denominator = int(f"1{('0' * (len(second) - 1))}")
+            return int(remove_str(str_num)) * int(TO_HUNDRED_MILLION / denominator)
 
         return int(remove_str(str_num)) * TO_HUNDRED_MILLION
 
@@ -72,5 +77,5 @@ def remove_str(str_num):
 
 if __name__ == '__main__':
     res = [x for x in open("./names.txt").readlines()]
-    total = open('total_price.txt').read()
+    total = open('total_money.txt').read()
     print(result(res, total))
