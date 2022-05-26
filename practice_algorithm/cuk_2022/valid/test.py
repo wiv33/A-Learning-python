5744031c30aeecd93167128db5fde8b082ad45b7
PSK <wiv821253@gmail.com>
2022-05-26 22:44:53 +0900

    add test case

---
+++ practice_algorithm/cuk_2022/valid/test.py
@@ -0,0 +1,35 @@
+import unittest
+import logging
+from ValidSolution import isValid
+
+
+class MyTestCase(unittest.TestCase):
+    def test_true(self):
+        true_list = ['()', "()[]{}", '((()))', '()', "([)]", "({[)]}", "{({[)]}}", "(){{[[]}}]"]
+        for i, x in enumerate(true_list):
+            try:
+                self.assertTrue(isValid(x))
+            except Exception as e:
+                print(i, x)
+                self.assertTrue(isValid(x))
+                self.assertIsNotNone(e)
+
+    def test_false(self):
+        false_list = ["((())", "(]", "((]}", "(}]}", "}{][", "{][}", "}{][", "(}){", "(}[){]", "({]})["]
+        for i, x in enumerate(false_list):
+            try:
+                self.assertFalse(isValid(x))
+            except Exception as e:
+                print(i, x)
+                self.assertTrue(isValid(x))
+                self.assertIsNotNone(e)
+
+    def test_temp_true(self):
+        self.assertTrue(isValid("([)]"))
+        self.assertTrue(isValid("({[})]"))
+        self.assertTrue(isValid("([{[})]]"))
+        self.assertTrue(isValid("{{{[[(})]}}]"))
+
+
+if __name__ == '__main__':
+    unittest.main()
