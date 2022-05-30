9630cd5cfa2fd959a6be3483857c5e7eda43b2ab
PSK <wiv821253@gmail.com>
2022-05-30 23:53:58 +0900

    모집 금액이 부족한 경우 오류 수정

--- sundry/ETC/FirstComeGroup.py
+++ sundry/ETC/FirstComeGroup.py
@@ -9,6 +9,8 @@ class OrderGroup:
         self.TO_MILLION: int = 1_000_000
         self.TO_TEN_THOUSAND: int = 10_000
         self.TO_WON: int = 1
+        self.get_most_mod = lambda: '모스트 나머지'
+        self.most_commission = 0.2
 
         self.unit_map = {'억': self.TO_HUNDRED_MILLION,
                          '천': self.TO_THOUSAND,
@@ -16,13 +18,14 @@ class OrderGroup:
                          '만': self.TO_TEN_THOUSAND,
                          '원': self.TO_WON}
         self.most_add = []
-        self.recruitment_money_groups = [self.recruitment_money_group(x.replace("\n", ''), i) for i, x in
+        self.recruitment_money_groups = [self.recruitment_money_group(name.replace("\n", '').split(" ")[0], name.replace("\n", '').split(" ")[1], idx) for
+                                         idx, name in
                                          enumerate(open('money_list.txt').readlines())]
         self.first_come_names = []
         self.filter_first_come_group()
-        self.expected_total_money = sum([int(x[0]) for x in self.recruitment_money_groups])
-        for i, k, price in self.most_add:
-            self.recruitment_money_groups.__getitem__(i)[1].append(("모스트", f"{price}"))
+        self.expected_total_money = sum([int(x[0]) for x in self.recruitment_money_groups]).__format__(',')
+        for idx, k, price in self.most_add:
+            self.recruitment_money_groups.__getitem__(idx)[1].append((self.get_most_mod(), f"{price}"))
 
     def filter_first_come_group(self):
         for x in open('names.txt').readlines():
@@ -31,6 +34,9 @@ class OrderGroup:
                 self.first_come_names.append(group)
 
     def to_regular_money(self, money, i=None):
+        if money == '':
+            return 0
+
         clean_money = re.sub('[^0-9억천백만원,.]', '', money)
         extract_unit = re.search('(백만)|[억천만원]', clean_money)
         if not extract_unit:
@@ -46,34 +52,33 @@ class OrderGroup:
     def clean_first_come_group(self, names):
         full_split = re.split('[ ]', names.replace('\n', ''))
 
-        name = ''.join(full_split[1:full_split.__len__() - 1]).split("/")[0]
         money = full_split[full_split.__len__() - 1]
         if not re.sub('[^0-9]', '', money).isdigit():
             return None
 
         # print(name, self.to_regular_money(money))
-        return name, self.to_regular_money(money)
+        return ''.join(full_split[1:full_split.__len__() - 1]).split("/")[0], self.to_regular_money(money)
 
-    def recruitment_money_group(self, money, i=None):
-        return self.to_regular_money(money, i), []
+    def recruitment_money_group(self, money, interest=0.003, i=None):
+        return self.to_regular_money(money, i), [], interest
 
     def result(self):
-        print(self.recruitment_money_groups)
-        print(self.expected_total_money)
-        print(self.first_come_names)
+        # print(self.recruitment_money_groups)
+        # print(self.expected_total_money)
+        # print(self.first_come_names)
 
-        for money, group in self.recruitment_money_groups:
+        for money, groups, interest in self.recruitment_money_groups:
             expected_to_money = 0
             most_add = []
-            for n, m in group:
+            for n, m in groups:
                 money -= int(m)
 
-            print(money)
             while expected_to_money < money:
                 if len(self.first_come_names) < 1:
-                    return 'first_come_names is empty'
+                    groups.append((self.get_most_mod(), money))
+                    return self.recruitment_money_groups
 
-                name, user_money = self.first_come_names.pop(0)
+                user_name, user_money = self.first_come_names.pop(0)
                 u_money = int(user_money)
                 expected_to_money = self.mod_most_add(expected_to_money, most_add)
 
@@ -81,16 +86,15 @@ class OrderGroup:
                 if money < sum_money:
                     final_money = money - expected_to_money
                     expected_to_money += final_money
-                    group.append((name, final_money))
-                    self.first_come_names.insert(0, (name, u_money - final_money))
-                    # print((name, final_money))
+                    groups.append((user_name, final_money))
+                    self.first_come_names.insert(0, (user_name, u_money - final_money))
                     break
 
                 expected_to_money += u_money
-                group.append((name, u_money))
+                groups.append((user_name, u_money))
 
             for most in most_add:
-                group.append(most)
+                groups.append(most)
 
         return self.recruitment_money_groups
 
@@ -99,17 +103,19 @@ class OrderGroup:
             mod_money = expected_to_money % self.unit_map['백만']
             most_mod_money = self.unit_map['백만'] - mod_money
             expected_to_money += most_mod_money
-            most_add.append(('모스트', most_mod_money))
+            most_add.append((self.get_most_mod(), most_mod_money))
         return expected_to_money
 
 
 if __name__ == '__main__':
-    group = OrderGroup()
-    for total_money, g in group.result():
-        print("=" * 33)
-        print(f"모집 금액: {format(total_money, ',')}")
-        for i, x in enumerate(g, 1):
-            print(f'{i}. {x[0]} {(int(x[1]).__format__(","))}')
+    orderGroup = OrderGroup()
+    print('모집 총 금액', orderGroup.expected_total_money)
+    print("=" * 33)
+    for result in orderGroup.result():
+        print(f"모집 금액: {format(result[0], ',')}")
+        for i, x in enumerate(result[1], 1):
+            name = x[0]
+            first_come_money = int(x[1])
+            print(f'{i}. {name} {(first_come_money.__format__(","))}')
 
         print("=" * 33)
-
