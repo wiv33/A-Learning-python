07f96433711b6314c58cc65c402140a39405d79e
PSK <wiv821253@gmail.com>
2022-11-28 09:47:40 +0900

    주석 추가

--- practice_algorithm/파이썬_인터뷰/_010_linked_list/_001_is_palindrome_runner.py
+++ practice_algorithm/파이썬_인터뷰/_010_linked_list/_001_is_palindrome_runner.py
@@ -10,6 +10,8 @@ def is_palindrome_runner(head: ListNode) -> bool:
 
     while fast and fast.next:
         fast = fast.next.next
+        # 오른쪽의 값 기준, 왼쪽 차례대로 대입 됨.
+        # 만약 처음 slow가 None이 되면 오류가 발생할 로직
         rev, rev.next, slow = slow, rev, slow.next
 
     if fast:
