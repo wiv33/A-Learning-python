677d84c809f35b93f4d4d47b37350ae1d8ca7dd1
PSK <wiv821253@gmail.com>
2025-04-22 12:41:37 +0900

    add: transpose 확인

--- sundry/test_hanyang.py
+++ sundry/test_hanyang.py
@@ -2,4 +2,7 @@ import pandas as pd
 
 df = pd.read_excel("/Users/auto/Documents/한양대_대학원/빅데이터기반_프로젝트/5장_데이터/01_시계열 분석 기초(시도별 주민등록인구).xlsx", engine='openpyxl')
 
-print(df.T.to_csv("hanyang.csv"))
+print(df.head())
+print(df.T.head())
+
+df.T.to_csv("hanyang.csv")
